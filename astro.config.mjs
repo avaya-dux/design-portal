f0cd34fd5a15091e58890cd595ed23fd62862fe3
Joe S <jsebast@avaya.com>
2022-08-10 12:03:26 -0700

    add netlify ish
    including `site` url for sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
+import netlify from "@astrojs/netlify/functions";
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
 import vue from "@astrojs/vue";
 
-// https://astro.build/config
 export default defineConfig({
+  adapter: netlify(),
   integrations: [react(), svelte(), vue()],
+  output: "server",
+  // site: "https://design.avayacloud.com/", // TODO: uncomment when we release v3
   vite: {
     server: {
       open: true,
