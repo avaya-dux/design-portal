0243cf8f78085bb3d9fd2dfe2c09fecd5181f317
Joe S <jsebast@avaya.com>
2022-09-14 07:49:22 -0700

    getting a whole bunch of config ish taken care of

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  tabWidth: 2,
+  bracketSpacing: true,
+  plugins: [require.resolve("prettier-plugin-astro")],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
