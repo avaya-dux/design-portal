9fc55babd19aacdf73514d07d6987f6ad1219124
Joe S <jsebast@avaya.com>
2022-09-14 14:07:07 -0700

    100% code coverage

--- src/components/react/Button.test.tsx
+++ src/components/react/Button.test.tsx
@@ -1,9 +1,14 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { axe } from "jest-axe";
+import { vi } from "vitest";
 
 import { Button } from "./Button";
 
 describe("Button", () => {
+  window.alert = () => null;
+  const user = userEvent.setup();
+
   it("fully renders without exploding", () => {
     render(<Button>Test</Button>);
 
@@ -11,8 +16,17 @@ describe("Button", () => {
     expect(rootElement).toBeDefined();
   });
 
+  it("accepts an `onclick` event which fires appropriately", async () => {
+    const clickSpy = vi.fn();
+    render(<Button onClick={clickSpy}>Test</Button>);
+    expect(clickSpy).not.toHaveBeenCalled();
+
+    await user.click(screen.getByRole("button"));
+    expect(clickSpy).toHaveBeenCalledTimes(1);
+  });
+
   it("passes basic axe compliance", async () => {
-    const { container } = render(<Button>Button</Button>);
+    const { container } = render(<Button>Test</Button>);
     const results = await axe(container);
     expect(results).toHaveNoViolations();
   });
