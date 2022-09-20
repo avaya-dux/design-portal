3be761f2cdf96270b04e91929742e70de0713730
Joe S <jsebast@avaya.com>
2022-09-20 16:23:51 -0700

    ugly but functional

--- src/components/astro/index.ts
+++ src/components/astro/index.ts
@@ -1,6 +1,7 @@
+import CodeWithCopy from "./CodeWithCopy.astro";
 import Description from "./Description.astro";
 import PageTitle from "./PageTitle.astro";
 import SectionHeader from "./SectionHeader.astro";
 import SiteFooter from "./SiteFooter.astro";
 
-export { Description, PageTitle, SectionHeader, SiteFooter };
+export { CodeWithCopy, Description, PageTitle, SectionHeader, SiteFooter };
