96b30331314606a7823f16f447271303e287fda2
Joe S <jsebast@avaya.com>
2022-09-22 11:04:03 -0700

    add notification and backup command

--- src/components/react/CodeHighlight/CodeHighlight.tsx
+++ src/components/react/CodeHighlight/CodeHighlight.tsx
@@ -1,8 +1,21 @@
-import { IconButton, Tooltip } from "@avaya/neo-react";
+import { IconButton, Notification, Tooltip } from "@avaya/neo-react";
+import { useState } from "react";
 
 import styles from "./CodeHighlight.module.css";
-
+/**
+ * The CodeHighlight component shows a code example and provides a "copy" (to clipboard) button.
+ *
+ * @example
+ * const react = `<Button>default</Button>`;
+ * return (<CodeHighlight code={react} />);
+ *
+ * @example
+ * // Astro implementation requires `client:load` for the button functionality to work.
+ * <CodeHighlight code="npm install @avaya/neo" client:load />
+ */
 export const CodeHighlight = ({ code }: { code: string }) => {
+  const [isCopied, setIsCopied] = useState(false);
+
   return (
     <div className={styles["code-container"]}>
       <code>{code}</code>
@@ -17,10 +30,33 @@ export const CodeHighlight = ({ code }: { code: string }) => {
           icon="copy"
           shape="circle"
           onClick={() => {
-            navigator.clipboard.writeText(code);
+            copyTextToClipboard(code);
+
+            setIsCopied(true);
+
+            setTimeout(() => {
+              setIsCopied(false);
+            }, 3000);
           }}
         />
       </Tooltip>
+
+      {isCopied && (
+        <Notification
+          action={{ onClick: () => setIsCopied(false) }}
+          header="Code copied to clipboard"
+          icon="copy"
+          type="success"
+        />
+      )}
     </div>
   );
+};
+
+const copyTextToClipboard = async (text: string) => {
+  if ("clipboard" in navigator) {
+    return await navigator.clipboard.writeText(text);
+  }
+
+  return document.execCommand("copy", true, text);
 };
