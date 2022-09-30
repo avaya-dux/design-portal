a1efc203aacb82c78dad5d321e8a28bae02cf8d3
Joe S <jsebast@avaya.com>
2022-09-30 15:07:07 -0700

    add DoAndDont test file

--- src/components/react/DoAndDont/DoAndDont.tsx
+++ src/components/react/DoAndDont/DoAndDont.tsx
@@ -27,6 +27,7 @@ export interface DoAndDontProps {
     </DoAndDont.DontContainer>
   </DoAndDont>
  *
+ * @example
  * <DoAndDont size="wide">
     <DoAndDont.DoContainer instructions={doInstructions}>
       <ImageExampleContainer>
