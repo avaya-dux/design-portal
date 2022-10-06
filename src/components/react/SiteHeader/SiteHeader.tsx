76d5529bff285c20b61d0d2f1efff780e9783a35
JoeS <55896546+joe-s-avaya@users.noreply.github.com>
2022-10-06 14:27:41 -0500

    update footer to respect new breakpoints  (#35)
    * fix some footer styles
    
    * cleanup some CSS
    
    * add css breakpoint variables in global `:root`
    
    * can't use css variables in `@media`... how strange
    
    * px to rem
    
    * reformat footer css from flex-grid to flex-grid
    
    * update several breakpoints
    
    * update site footer to have a "mobile" view
    
    * fix typo
    
    Co-authored-by: Joe S <jsebast@avaya.com>

--- src/components/react/SiteHeader/SiteHeader.tsx
+++ src/components/react/SiteHeader/SiteHeader.tsx
@@ -1,7 +1,10 @@
 import type { AstroInstance } from "astro";
 import { TopNav } from "@avaya/neo-react";
 import { useCallback } from "react";
+
 import { TopNavSearch } from "./helpers";
+
+import "./SiteHeaderStyleOverrides.css";
 
 export interface PageAstroInstance extends AstroInstance {
   title: string;
@@ -88,23 +91,20 @@ const Logo = () => (
   <a href="/" aria-label="Homepage">
     <picture>
       <source
-        media="(max-width: 1024px) and (prefers-color-scheme: dark)"
+        media="(max-width: 799px) and (prefers-color-scheme: dark)"
         srcSet="/imgs/logo-mobile-dark.svg"
       />
+      <source media="(max-width: 799px)" srcSet="/imgs/logo-mobile-light.svg" />
       <source
-        media="(max-width: 1024px)"
-        srcSet="/imgs/logo-mobile-light.svg"
-      />
-      <source
-        media="(max-width: 1440px) and (prefers-color-scheme: dark)"
+        media="(max-width: 1279px) and (prefers-color-scheme: dark)"
         srcSet="/imgs/logo-condensed-dark.svg"
       />
       <source
-        media="(max-width: 1440px)"
+        media="(max-width: 1279px)"
         srcSet="/imgs/logo-condensed-light.svg"
       />
       <source
-        media="(min-width: 1441px) and (prefers-color-scheme: dark)"
+        media="(min-width: 1280px) and (prefers-color-scheme: dark)"
         srcSet="/imgs/logo-full-dark.svg"
       />
       <img src="/imgs/logo-full-light.svg" alt="Avaya Logo" />
