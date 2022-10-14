51296d36250d3c69d03eb4df89176bd8b5aeddff
Enrique Prado <enriquep@avaya.com>
2022-10-13 17:27:18 -0700

    Implementing Topics component

--- src/components/astro/index.ts
+++ src/components/astro/index.ts
@@ -2,5 +2,6 @@ import Description from "./Description.astro";
 import PageTitle from "./PageTitle.astro";
 import SectionHeader from "./SectionHeader.astro";
 import SiteFooter from "./SiteFooter.astro";
+import Topics from "./Topics.astro";
 
-export { Description, PageTitle, SectionHeader, SiteFooter };
+export { Description, PageTitle, SectionHeader, SiteFooter, Topics };
