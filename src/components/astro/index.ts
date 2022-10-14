a2372ec00bb11eb42fe6a0b8078d3bdf5c02904b
Enrique Prado <enriquep@avaya.com>
2022-10-14 09:54:45 -0700

    Implemented TopicLink astro component

--- src/components/astro/index.ts
+++ src/components/astro/index.ts
@@ -2,6 +2,7 @@ import Description from "./Description.astro";
 import PageTitle from "./PageTitle.astro";
 import SectionHeader from "./SectionHeader.astro";
 import SiteFooter from "./SiteFooter.astro";
+import TopicLink from "./TopicLink.astro";
 import Topics from "./Topics.astro";
 
-export { Description, PageTitle, SectionHeader, SiteFooter, Topics };
+export { Description, PageTitle, SectionHeader, SiteFooter, TopicLink, Topics };
