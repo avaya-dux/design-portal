3d505f1e3f702603175073fbfa519d8aaff3c2d3
Joe S <jsebast@avaya.com>
2022-10-20 14:17:16 -0700

    remove `PageSection` as Enrique is currently refactoring it

--- src/components/astro/index.ts
+++ src/components/astro/index.ts
@@ -1,17 +1,8 @@
 import Description from "./Description.astro";
 import PageTitle from "./PageTitle.astro";
-import PageSection from "./PageSection.astro";
 import SectionHeader from "./SectionHeader.astro";
 import SiteFooter from "./SiteFooter.astro";
 import TopicLink from "./TopicLink.astro";
 import Topics from "./Topics.astro";
 
-export {
-  Description,
-  PageTitle,
-  PageSection,
-  SectionHeader,
-  SiteFooter,
-  TopicLink,
-  Topics,
-};
+export { Description, PageTitle, SectionHeader, SiteFooter, TopicLink, Topics };
