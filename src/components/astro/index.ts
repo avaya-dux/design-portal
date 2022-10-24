e98ae9c9f70bf3415a7217992db6a0c8b3ae852b
Enrique Prado <enriquep@avaya.com>
2022-10-24 15:54:16 -0700

    Added new ColumnLayout component

--- src/components/astro/index.ts
+++ src/components/astro/index.ts
@@ -1,9 +1,9 @@
 import Description from "./Description.astro";
-import ImageContainer from "./ImageContainer.astro";
+import ColumnLayout from "./ColumnLayout.astro";
 import PageTitle from "./PageTitle.astro";
 import SectionHeader from "./SectionHeader.astro";
 import SiteFooter from "./SiteFooter.astro";
 import TopicLink from "./TopicLink.astro";
 import Topics from "./Topics.astro";
 
-export { Description, ImageContainer, PageTitle, SectionHeader, SiteFooter, TopicLink, Topics };
+export { Description, ColumnLayout, PageTitle, SectionHeader, SiteFooter, TopicLink, Topics };
