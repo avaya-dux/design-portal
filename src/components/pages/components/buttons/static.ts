a3b704b755f682fa2f2f9ae9c181ee3ca06ceb06
Mo Razian <mrazian@avaya.com>
2022-10-25 10:58:50 -0400

    Re-factored page anchors into static.ts file for each Component

--- src/components/pages/components/buttons/static.ts
+++ src/components/pages/components/buttons/static.ts
@@ -6,3 +6,15 @@ export const defaultReact = `<Button>default</Button>`;
 export const sandbox = "https://codesandbox.io/s/neo-react-button-qoluzy";
 export const storybook =
   "https://neo-react-library-storybook.netlify.app/?path=/story/components-button";
+export const buttonPageAnchors = {
+  playground: "button-playground",
+  anatomy: "button-anatomy",
+  arrangement: "button-arrangement",
+  type: "button-type",
+  "text-label": "button-text-label",
+  states: "button-states",
+  behavior: "button-behavior",
+  "using-icons": "button-using-icons",
+  specs: "button-specs",
+  "keyboard-interactions": "button-keyboard-interactions",
+};
--- src/components/pages/components/spinner/static.ts
+++ src/components/pages/components/spinner/static.ts
@@ -3,3 +3,6 @@ export const defaultReact = `<Spinner />`;
 export const sandbox = "https://codesandbox.io/s/neo-react-spinner-yj5o5i";
 export const storybook =
   "https://neo-react-library-storybook.netlify.app/?path=/story/components-spinner--default";
+export const spinnerPageAnchors = {
+  playground: "spinner-playground",
+};
