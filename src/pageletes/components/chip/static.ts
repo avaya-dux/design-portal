53306d36ed35185013083df4538869013d501ea2
Joe S <jsebast@avaya.com>
2022-10-26 14:17:31 -0700

    simplify Topic Links

--- src/pageletes/components/chip/static.ts
+++ src/pageletes/components/chip/static.ts
@@ -5,16 +5,25 @@ export const storybook =
   "https://neo-react-library-storybook.netlify.app/?path=/story/components-chips-basic-chip";
 
 export const anchors = {
-  avatar: { id: "avatar-chips", title: "Chips With Avatar" },
-  basic: { id: "basic-chips", title: "Basic Chips" },
-  closable: { id: "closable-icon-chips", title: "Chips With Close Icon" },
-  container: { id: "chips-container", title: "Chips Container" },
-  disabled: { id: "disabled-chips", title: "Disabled Chips" },
-  expandable: { id: "expandable-chips", title: "Expandable Chips" },
-  icon: { id: "icon-chips", title: "Chips With Icons" },
+  avatar: { order: 7, id: "avatar-chips", title: "Chips With Avatar" },
+  basic: { order: 2, id: "basic-chips", title: "Basic Chips" },
+  closable: {
+    order: 5,
+    id: "closable-icon-chips",
+    title: "Chips With Close Icon",
+  },
+  container: { order: 8, id: "chips-container", title: "Chips Container" },
+  disabled: { order: 3, id: "disabled-chips", title: "Disabled Chips" },
+  expandable: { order: 6, id: "expandable-chips", title: "Expandable Chips" },
+  icon: { order: 4, id: "icon-chips", title: "Chips With Icons" },
   keyboard: {
+    order: 9,
     id: "chips-keyboard-interactions",
     title: "Keyboard Interactions",
   },
-  playground: { id: "chips-playground", title: "Interactive Playground" },
+  playground: {
+    order: 1,
+    id: "chips-playground",
+    title: "Interactive Playground",
+  },
 };
