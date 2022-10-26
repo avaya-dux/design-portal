44dd0fa176a0e2928c19c941f7f0e34a9140abc2
Enrique Prado <enriquep@avaya.com>
2022-10-26 15:29:51 -0700

    Adding InfoTable components and subcomponents

--- src/components/astro/index.ts
+++ src/components/astro/index.ts
@@ -1,5 +1,8 @@
 import Description from "./Description.astro";
 import ColumnLayout from "./ColumnLayout.astro";
+import InfoTable from "./InfoTable";
+import InfoTableHeader from "./InfoTableHeader";
+import InfoTableRow from "./InfoTableRow";
 import PageTitle from "./PageTitle.astro";
 import SectionHeader from "./SectionHeader.astro";
 import SiteFooter from "./SiteFooter.astro";
@@ -9,6 +12,9 @@ import Topics from "./Topics.astro";
 export {
   Description,
   ColumnLayout,
+  InfoTable,
+  InfoTableHeader,
+  InfoTableRow,
   PageTitle,
   SectionHeader,
   SiteFooter,
