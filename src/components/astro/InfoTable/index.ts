99c06ee79f09d76dc158311b671dbf70a57d42ce
Joe S <jsebast@avaya.com>
2022-10-27 14:53:45 -0700

    remove dead code, add a touch of styling

--- src/components/astro/InfoTable/index.ts
+++
@@ -1,5 +0,0 @@
-import InfoTable from "./InfoTable.astro";
-import InfoTableHeader from "./InfoTableHeader.astro";
-import InfoTableRow from "./InfoTableRow.astro";
-
-export { InfoTable, InfoTableHeader, InfoTableRow };
--- src/components/astro/index.ts
+++ src/components/astro/index.ts
@@ -1,5 +1,6 @@
 import ColumnLayout from "./ColumnLayout.astro";
 import Description from "./Description.astro";
+import InfoTable from "./InfoTable.astro";
 import PageTitle from "./PageTitle.astro";
 import SectionHeader from "./SectionHeader.astro";
 import SiteFooter from "./SiteFooter.astro";
@@ -9,11 +10,10 @@ import Topics from "./Topics.astro";
 export {
   ColumnLayout,
   Description,
+  InfoTable,
   PageTitle,
   SectionHeader,
   SiteFooter,
   TopicLink,
   Topics,
 };
-
-export * from "./InfoTable";
