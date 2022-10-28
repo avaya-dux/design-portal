5e719c84ca5ab97e5bee55d2fb24b48681639395
razianmm <raziandev@gmail.com>
2022-10-28 15:36:05 -0400

    Updated Storybook link, added note for sandbox

--- src/pageletes/components/toast/static.ts
+++ src/pageletes/components/toast/static.ts
@@ -1,7 +1,8 @@
 export const defaultReact = `<Toast position={"top"} duration={10000}>This is a toast</Toast>`;
+// TO-DO: Create sandbox link
 export const sandbox = "https://codesandbox.io/s/neo-react-spinner-yj5o5i";
 export const storybook =
-  "https://neo-react-library-storybook.netlify.app/?path=/story/components-spinner--default";
+  "https://neo-react-library-storybook.netlify.app/?path=/story/components-toast--default";
 export const anchors = {
   playground: "toast-playground",
 };
