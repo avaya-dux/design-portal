7db2bc8d3e2f717285bee2c0bebff60c205b8fbc
razianmm <raziandev@gmail.com>
2022-10-28 15:31:16 -0400

    Added Toast page

---
+++ src/pageletes/components/toast/static.ts
@@ -0,0 +1,7 @@
+export const defaultReact = `<Toast position={"top"} duration={10000}>This is a toast</Toast>`;
+export const sandbox = "https://codesandbox.io/s/neo-react-spinner-yj5o5i";
+export const storybook =
+  "https://neo-react-library-storybook.netlify.app/?path=/story/components-spinner--default";
+export const anchors = {
+  playground: "toast-playground",
+};
