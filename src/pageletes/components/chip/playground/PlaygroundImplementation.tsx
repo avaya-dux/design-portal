aab84b82c74e03086419e9d4e83e2190bc3b365c
JoeS <55896546+joe-s-avaya@users.noreply.github.com>
2022-10-28 15:47:30 -0500

    Chips Page, part 1 (#54)
    * get the structure in
    
    * add all sections
    
    * simplify Topic Links
    
    * start refactor
    
    * better notes
    
    * resolve name change issues
    
    * get placeholders in for playground impl
    
    * update per PR suggestions
    
    * format
    
    Co-authored-by: Joe S <jsebast@avaya.com>

---
+++ src/pageletes/components/chip/playground/PlaygroundImplementation.tsx
@@ -0,0 +1,31 @@
+import toDiffableHtml from "diffable-html";
+
+import { Playground } from "components";
+
+import { defaultHtml, defaultReact, sandbox, storybook } from "../static";
+
+export const PlaygroundImplementation = () => {
+  return (
+    <Playground
+      options={
+        <Playground.OptionsContainer>
+          <Playground.OptionsSection title="Chip Options">
+            None
+          </Playground.OptionsSection>
+        </Playground.OptionsContainer>
+      }
+      examples={{
+        html: toDiffableHtml(defaultHtml),
+        react: defaultReact,
+        sandbox,
+        storybook,
+      }}
+    >
+      <div className="neo-chip neo-chip--default">This</div>
+      <div className="neo-chip neo-chip--success">is</div>
+      <div className="neo-chip neo-chip--info">a</div>
+      <div className="neo-chip neo-chip--alert">placeholder</div>
+      <div className="neo-chip neo-chip--warning">example</div>
+    </Playground>
+  );
+};
---
+++ src/pageletes/components/chip/sections/index.ts
@@ -0,0 +1,29 @@
+import Intro from "./Intro.astro";
+export { Intro };
+
+import ChipPlayground from "./ChipPlayground.astro";
+export { ChipPlayground };
+
+import BasicChips from "./BasicChips.astro";
+export { BasicChips };
+
+import DisabledChips from "./DisabledChips.astro";
+export { DisabledChips };
+
+import IconChips from "./IconChips.astro";
+export { IconChips };
+
+import ClosableChips from "./ClosableChips.astro";
+export { ClosableChips };
+
+import ExpandableChips from "./ExpandableChips.astro";
+export { ExpandableChips };
+
+import AvatarChips from "./AvatarChips.astro";
+export { AvatarChips };
+
+import ChipsContainer from "./ChipsContainer.astro";
+export { ChipsContainer };
+
+import KeyboardInteractions from "./KeyboardInteractions.astro";
+export { KeyboardInteractions };
