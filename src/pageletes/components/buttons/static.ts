81220bd76e051f335e4b23d00790d0c231a9416a
JoeS <55896546+joe-s-avaya@users.noreply.github.com>
2022-10-31 11:34:53 -0500

    use new `topics` format for all pageletes, remove dead code (#59)
    * use new `topics` format for all pageletes, remove dead code
    
    * fix `order` issue in Buttons page
    
    Co-authored-by: Joe S <jsebast@avaya.com>

--- src/pageletes/components/buttons/static.ts
+++ src/pageletes/components/buttons/static.ts
@@ -1,3 +1,5 @@
+import type { TopicsDict } from "components";
+
 export const defaultHtml = `<button class="neo-btn neo-btn-primary neo-btn-primary--default">
   default
 </button>
@@ -6,9 +8,38 @@ export const defaultReact = `<Button>default</Button>`;
 export const sandbox = "https://codesandbox.io/s/neo-react-button-qoluzy";
 export const storybook =
   "https://neo-react-library-storybook.netlify.app/?path=/story/components-button";
-export const anchors = {
-  playground: "button-playground",
-  arrangement: "button-arrangement",
-  anatomy: "button-anatomy",
-  textLabel: "button-text-label",
+
+type ButtonsTopicNames =
+  | "playground"
+  | "anatomy"
+  | "textLabel"
+  | "arrangement"
+  | "type";
+
+export const topics: TopicsDict<ButtonsTopicNames> = {
+  playground: {
+    order: 0,
+    id: "button-playground",
+    title: "Interactive Playground",
+  },
+  anatomy: {
+    order: 1,
+    id: "anatomy-playground",
+    title: "Anatomy",
+  },
+  type: {
+    order: 2,
+    id: "button-type",
+    title: "Type",
+  },
+  textLabel: {
+    order: 3,
+    id: "text-label-playground",
+    title: "Text Label",
+  },
+  arrangement: {
+    order: 4,
+    id: "arrangement-playground",
+    title: "Arrangement",
+  },
 };
