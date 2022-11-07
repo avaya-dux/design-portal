a40d6798fe74066401b7f1ad8ba6811cdb155e50
mrazian85 <mrazian@avaya.com>
2022-11-07 14:49:51 -0500

    Renamed helper function to getOSName

--- src/components/react/SiteHeader/helpers/TopNavSearch.tsx
+++ src/components/react/SiteHeader/helpers/TopNavSearch.tsx
@@ -1,7 +1,7 @@
 import { TextInput, Button } from "@avaya/neo-react";
 import { useEffect, useRef, useState } from "react";
 
-import { OsName } from "components/react/utils";
+import { getOSName } from "components/react/utils";
 
 import { TopNavSearchModal } from "./TopNavSearchModal/TopNavSearchModal";
 
@@ -32,7 +32,7 @@ export const TopNavSearch = ({ pages }: { pages: PageAstroInstance[] }) => {
   const searchModalRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    setOs(OsName(window.navigator.userAgent.toLowerCase()));
+    setOs(getOSName(window.navigator.userAgent.toLowerCase()));
   }, []);
 
   useEffect(() => {
--- src/components/react/utils/helpers.ts
+++ src/components/react/utils/helpers.ts
@@ -1,7 +1,7 @@
 export const copyTextToClipboard = async (text: string) =>
   await navigator.clipboard.writeText(text);
 
-export const OsName = (userAgent: string) => {
+export const getOSName = (userAgent: string) => {
   const macPlatforms = /(macintosh|macintel|macppc|mac68k|macos)/i;
 
   return macPlatforms.test(userAgent) ? "macOS" : "windows";
