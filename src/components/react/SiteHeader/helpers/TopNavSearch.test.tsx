b794db716e8c9d11aec801c19508322b4ab1c28f
mrazian85 <61242168+mrazian85@users.noreply.github.com>
2022-11-07 17:12:26 -0500

    NEO-1246: Search Part #1 - Search Modal basic functionality (#67)
    * Continued building search modal
    
    * Start separating out Components into different files
    
    * Continue building search modal
    
    * Continue building Search Modal
    
    * Formatting changes after yarn all
    
    * Remove previous tests after changing functionality
    
    * Removed non-null assertion
    
    * Reworked keyboard navigation
    
    * Made TopNav Search shortcut buttons un-tabbable
    
    * Apply suggestions from code review
    
    Committing batched suggestions from PR review
    
    Co-authored-by: JoeS <55896546+joe-s-avaya@users.noreply.github.com>
    
    * Re-factored code to change the way refs are created in TopNavSearchModalResults as per suggestion in PR feedback
    
    * Picked up dev feedback before starting on design feedback
    
    * Added autofocus prop to TextInput in search modal
    
    * Remove autofocus prop from TextInput as it caused build errors
    
    * Changed a11y linter rules to disable strict disallow of autofocus, added autofocus prop back to TextInput
    
    * Made visual changes as per design feedback on PR
    
    * Re-factored function to detect OS as per feedback to improve testability; function is also renamed and is no longer a hook
    
    * Changed name of function to detect OS again to remove 'use'
    
    * Batching further suggested changes from PR review
    
    Co-authored-by: JoeS <55896546+joe-s-avaya@users.noreply.github.com>
    
    * Renamed prop in topNavSearchOnKeyUp as per suggestion in PR feedback, reverted from single call to state to multiple on key handlers
    
    * Renamed helper function to getOSName
    
    * Removed OS as passed-in prop to openModal keybaord handler
    
    * Fixed method name in implementation
    
    * Update src/components/react/SiteHeader/helpers/TopNavSearchKeyboardHandlers.ts
    
    Co-authored-by: JoeS <55896546+joe-s-avaya@users.noreply.github.com>
    
    * Began re-working search results link navigation keyboard handler as per feedback
    
    * Tweaked padding on search results modal
    
    * Fixed padding between up/down arrow buttons in Modal
    
    Co-authored-by: JoeS <55896546+joe-s-avaya@users.noreply.github.com>

--- src/components/react/SiteHeader/helpers/TopNavSearch.test.tsx
+++ src/components/react/SiteHeader/helpers/TopNavSearch.test.tsx
@@ -1,5 +1,4 @@
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { render } from "@testing-library/react";
 
 import type { AstroComponentFactory } from "astro/dist/runtime/server";
 import { axe } from "jest-axe";
@@ -8,10 +7,6 @@ import { TopNavSearch } from ".";
 import type { PageAstroInstance } from "../";
 
 describe("TopNavSearch", () => {
-  const user = userEvent.setup();
-
-  const searchRole = "searchbox";
-  const searchResultQuery = "[role='dialog'] a";
   const pagesMockData: PageAstroInstance[] = [
     {
       url: "",
@@ -36,78 +31,9 @@ describe("TopNavSearch", () => {
     },
   ];
 
-  it("fully renders without exploding", () => {
-    render(<TopNavSearch pages={pagesMockData} />);
-
-    const searchElement = screen.getByRole(searchRole);
-    expect(searchElement).toBeDefined();
-  });
-
   it("passes basic axe compliance", async () => {
     const { container } = render(<TopNavSearch pages={pagesMockData} />);
     const results = await axe(container);
     expect(results).toHaveNoViolations();
-  });
-
-  describe("Search functionality", () => {
-    it("should not show search results when search input is empty", () => {
-      const { container } = render(<TopNavSearch pages={pagesMockData} />);
-
-      expect(container.querySelector(searchResultQuery)).toBeNull();
-    });
-
-    it("should not show search results when search returns no results", async () => {
-      const { container } = render(<TopNavSearch pages={pagesMockData} />);
-
-      const searchInput = screen.getByRole(searchRole);
-      await user.type(searchInput, "asdfasdfasdf");
-
-      expect(container.querySelector(searchResultQuery)).toBeNull();
-    });
-
-    it("should show search results when search input is not empty", async () => {
-      const { container } = render(<TopNavSearch pages={pagesMockData} />);
-
-      const searchInput = screen.getByRole(searchRole);
-      await user.type(searchInput, "home");
-
-      expect(container.querySelector(searchResultQuery)).not.toBeNull();
-    });
-
-    it("allows tabbing to search results", async () => {
-      render(<TopNavSearch pages={pagesMockData} />);
-
-      const searchInput = screen.getByRole(searchRole);
-      await user.type(searchInput, "home");
-
-      await user.tab(); // tab to "clear" button
-      await user.tab(); // tab to first search result
-
-      expect(screen.getByRole("link", { name: "Home" })).toHaveFocus();
-    });
-
-    it("closes `Sheet` when search is made empty", async () => {
-      const { container } = render(<TopNavSearch pages={pagesMockData} />);
-
-      const searchInput = screen.getByRole(searchRole);
-
-      await user.type(searchInput, "home");
-      expect(container.querySelector(searchResultQuery)).not.toBeNull();
-
-      await user.clear(searchInput);
-      expect(container.querySelector(searchResultQuery)).toBeNull();
-    });
-
-    it("closes `Sheet` when 'Close' button is clicked", async () => {
-      const { container } = render(<TopNavSearch pages={pagesMockData} />);
-
-      const searchInput = screen.getByRole(searchRole);
-      await user.type(searchInput, "home");
-      expect(container.querySelector(searchResultQuery)).not.toBeNull();
-
-      const closeButton = screen.getByRole("button", { name: "Close" });
-      await user.click(closeButton);
-      expect(container.querySelector(searchResultQuery)).toBeNull();
-    });
   });
 });
