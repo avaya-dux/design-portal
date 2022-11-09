e85098342be3690e262c9496190eaacd12d0d1c8
mrazian85 <mrazian@avaya.com>
2022-11-09 14:55:35 -0500

    Cleaned up test file

--- src/components/react/SiteHeader/helpers/TopNavSearchMouseHandlers.test.ts
+++ src/components/react/SiteHeader/helpers/TopNavSearchMouseHandlers.test.ts
@@ -1,22 +1,6 @@
-import { createElement } from "react";
 import { vi } from "vitest";
 
 import { closeSearchModalOnClick } from "./TopNavSearchMouseHandlers";
-
-// export const closeSearchModalOnClick = (
-//   event: MouseEvent,
-//   isOpen: boolean,
-//   openModal: React.Dispatch<React.SetStateAction<boolean>>,
-//   modalRef: React.RefObject<HTMLDivElement>
-// ) => {
-//   if (isOpen && modalRef.current && event.target instanceof HTMLElement) {
-//     if (
-//       !modalRef.current.contains(event.target) &&
-//       !event.target.classList.contains("neo-modal__content")
-//     )
-//       openModal(false);
-//   }
-// };
 
 describe("TopNav Search Mouse Handlers", () => {
   const ref: any = { current: document.createElement("div") };
