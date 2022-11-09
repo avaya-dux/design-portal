bebc6f9d3e195a8bdaab5fc28c0742de61d84a91
mrazian85 <mrazian@avaya.com>
2022-11-09 11:04:22 -0500

    Added unit test for custom useOsName hook

--- src/components/react/utils/hooks.test.ts
+++ src/components/react/utils/hooks.test.ts
@@ -1,10 +1,28 @@
-import { useOsName } from './hooks'
+import { renderHook } from "@testing-library/react";
+import { useOsName } from "./hooks";
 
+describe("custom hooks", () => {
+  describe("useOsName", () => {
+    it("returns the correct value when user agent string is macos", () => {
 
-(<any>navigator)['__defineGetter__']('userAgent', function(){
-  return 'foo';
+      (<any>navigator)["__defineGetter__"]("userAgent", function () {
+        return "macos";
+      });
+
+      const { result } = renderHook(() => useOsName());
+
+      expect(result.current).toBe("macos");
+    });
+
+    it("returns the correct value when user agent string is not macos", () => {
+
+      (<any>navigator)["__defineGetter__"]("userAgent", function () {
+        return "linux";
+      });
+
+      const { result } = renderHook(() => useOsName());
+
+      expect(result.current).toBe("windows");
+    });
+  });
 });
-
-navigator.userAgent; // 'foo'
-
-describe("")
--- src/components/react/utils/hooks.ts
+++ src/components/react/utils/hooks.ts
@@ -1,15 +1,15 @@
 import { useEffect, useState } from "react";
 
 export const useOsName = () => {
-  const [Os, setOs] = useState<string>("");
+  const [os, setOs] = useState<string>("");
 
   const macPlatforms = /(macintosh|macintel|macppc|mac68k|macos)/i;
 
   useEffect(() => {
     const userAgent = window.navigator.userAgent.toLowerCase();
 
-    setOs(macPlatforms.test(userAgent) ? "macOS" : "windows");
+    setOs(macPlatforms.test(userAgent) ? "macos" : "windows");
   }, []);
 
-  return Os;
+  return os;
 };
