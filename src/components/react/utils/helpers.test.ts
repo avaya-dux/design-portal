7e98936a0f01d88b4c52afd2e4aa0e86ec26da16
mrazian85 <mrazian@avaya.com>
2022-11-15 18:38:12 +0100

    Added unit test for disable scroll method

---
+++ src/components/react/utils/helpers.test.ts
@@ -0,0 +1,20 @@
+import { vi } from "vitest";
+
+import { disableScroll } from "./helpers";
+
+describe("disableScroll", () => {
+  const spyScroll = vi.fn();
+  Object.defineProperty(window, "scroll", { value: spyScroll });
+
+  it("fires the correct event when the appropiate values are passed in", () => {
+    disableScroll(true, 320, 799);
+
+    expect(window.scroll).toHaveBeenCalled();
+  });
+
+  it("does not fire when the appropiate values are passed in", () => {
+    disableScroll(false, 320, 799);
+
+    expect(window.scroll).not.toHaveBeenCalledTimes(2);
+  });
+});
