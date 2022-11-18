a899b6cb0f4fec856777bb885105d23509c91ded
Joe S <jsebast@avaya.com>
2022-11-18 13:10:00 -0800

    touch up some styling

--- src/pageletes/components/accordion/sections/index.ts
+++ src/pageletes/components/accordion/sections/index.ts
@@ -4,5 +4,8 @@ export { Intro };
 import AccordionPlayground from "./AccordionPlayground.astro";
 export { AccordionPlayground };
 
+import Anatomy from "./Anatomy.astro";
+export { Anatomy };
+
 import KeyboardInteractions from "./KeyboardInteractions.astro";
 export { KeyboardInteractions };
