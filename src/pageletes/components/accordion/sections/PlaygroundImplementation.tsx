1f7b7d3ec771b002503d2fd376d99084371cf6e1
Joe S <jsebast@avaya.com>
2022-11-18 09:35:46 -0800

    add placeholder for playground

---
+++ src/pageletes/components/accordion/sections/PlaygroundImplementation.tsx
@@ -0,0 +1,90 @@
+import {
+  Accordion,
+  AccordionGroup,
+  Checkbox,
+  CheckboxGroup,
+  Radio,
+  RadioGroup,
+} from "@avaya/neo-react";
+import { useState } from "react";
+
+import { Playground } from "components";
+
+import { defaultHtml, defaultReact, sandbox, storybook } from "../static";
+
+type TypeOption = "single" | "group";
+
+export const PlaygroundImplementation = () => {
+  const [typeOption, setTypeOption] = useState<TypeOption>("single");
+  const [open, setOpen] = useState(false);
+  const [disabled, setDisabled] = useState(false);
+
+  return (
+    <Playground
+      options={
+        <Playground.OptionsContainer>
+          <Playground.OptionsSection title="Types">
+            <RadioGroup
+              groupName="type-options"
+              selected={typeOption}
+              onChange={(e) => {
+                setTypeOption(e.target.value as TypeOption);
+              }}
+            >
+              <Radio value="single">Single</Radio>
+              <Radio value="group">Group</Radio>
+            </RadioGroup>
+          </Playground.OptionsSection>
+
+          <Playground.OptionsSection
+            title="Variables"
+            htmlFor="Variables"
+            className="hack-hide-me"
+          >
+            <CheckboxGroup
+              groupName="Variables"
+              onChange={(e) => {
+                const { value } = e.target as HTMLInputElement;
+                switch (value) {
+                  case "open":
+                    setOpen(!open);
+                    break;
+                  case "disabled":
+                    setDisabled(!disabled);
+                    break;
+                }
+              }}
+            >
+              <Checkbox label="Open" value="open" checked={open} />
+              <Checkbox label="Disabled" value="disabled" checked={disabled} />
+            </CheckboxGroup>
+          </Playground.OptionsSection>
+        </Playground.OptionsContainer>
+      }
+      examples={{
+        html: defaultHtml,
+        react: defaultReact,
+        sandbox,
+        storybook,
+      }}
+    >
+      {typeOption === "single" ? (
+        <Accordion header="Single Accordion Example">
+          Inner content of Accordion example
+        </Accordion>
+      ) : (
+        <AccordionGroup>
+          <Accordion header="Accordion Group Example 1">
+            Inner content of Accordion example
+          </Accordion>
+          <Accordion header="Accordion Group Example 2">
+            Inner content of Accordion example
+          </Accordion>
+          <Accordion header="Accordion Group Example 3">
+            Inner content of Accordion example
+          </Accordion>
+        </AccordionGroup>
+      )}
+    </Playground>
+  );
+};
--- src/pageletes/components/accordion/sections/index.ts
+++ src/pageletes/components/accordion/sections/index.ts
@@ -1,5 +1,8 @@
 import Intro from "./Intro.astro";
 export { Intro };
 
+import AccordionPlayground from "./AccordionPlayground.astro";
+export { AccordionPlayground };
+
 import KeyboardInteractions from "./KeyboardInteractions.astro";
 export { KeyboardInteractions };
