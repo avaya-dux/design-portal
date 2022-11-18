67450b684abe946f2283d95dfba2b61de2c9e074
Joe S <jsebast@avaya.com>
2022-11-18 09:06:21 -0800

    add the basics for Accordion

---
+++ src/pageletes/components/accordion/sections/index.ts
@@ -0,0 +1,5 @@
+import Intro from "./Intro.astro";
+export { Intro };
+
+import KeyboardInteractions from "./KeyboardInteractions.astro";
+export { KeyboardInteractions };
---
+++ src/pageletes/components/accordion/static.ts
@@ -0,0 +1,49 @@
+import type { TopicsDict } from "components";
+import { prettyPrintHtml, prettyPrintReact } from "helpers";
+
+export const defaultHtml = prettyPrintHtml(``);
+export const defaultReact = prettyPrintReact(``);
+
+export const sandbox =
+  "https://codesandbox.io/s/neo-react-accodion-m7zsdz?file=/src/App.js";
+export const storybook =
+  "https://neo-react-library-storybook.netlify.app/?path=/story/components-accordion";
+
+type AccordionTopicNames =
+  | "playground"
+  | "anatomy"
+  | "states"
+  | "usage"
+  | "specs"
+  | "keyboard"
+  | "related";
+
+export const topics: TopicsDict<AccordionTopicNames> = {
+  playground: {
+    order: 0,
+    id: "accordions-playground",
+    title: "Interactive Playground",
+  },
+  anatomy: { order: 1, id: "accordion-anatomy", title: "Anatomy" },
+  states: {
+    order: 2,
+    id: "accordions-states",
+    title: "States",
+  },
+  usage: { order: 3, id: "accordions-usage", title: "Usages" },
+  specs: {
+    order: 4,
+    id: "accordion-specs",
+    title: "Specs",
+  },
+  keyboard: {
+    order: 5,
+    id: "accordions-keyboard-interactions",
+    title: "Keyboard Interactions",
+  },
+  related: {
+    order: 6,
+    id: "accordions-related-content",
+    title: "Related Content",
+  },
+};
