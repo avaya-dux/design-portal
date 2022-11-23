56bfb42614bd0743ca3ce1afa384ef2900e5852d
Enrique Prado <enriquep@avaya.com>
2022-11-23 12:36:52 -0800

    Added Related Content section

--- src/pageletes/components/badge/index.ts
+++ src/pageletes/components/badge/index.ts
@@ -4,5 +4,8 @@ export { Intro };
 import Anatomy from "./Anatomy.astro";
 export { Anatomy };
 
+import RelatedContent from "./RelatedContent.astro";
+export { RelatedContent };
+
 import Usage from "./Usage.astro";
 export { Usage };
