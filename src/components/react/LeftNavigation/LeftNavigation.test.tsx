fbc5d42a60a3449819acaaa441361da35c400a12
mrazian85 <mrazian@avaya.com>
2022-12-09 10:17:36 -0500

    Added test for LeftNavigation

---
+++ src/components/react/LeftNavigation/LeftNavigation.test.tsx
@@ -0,0 +1,83 @@
+import { act, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { axe } from "jest-axe";
+import { cleanStores, keepMount } from "nanostores";
+
+import { isLeftNavigationOpen } from "helpers/layoutState";
+
+import { LeftNavigation } from ".";
+import { pagesMockData } from "../SiteHeader/helpers/mocks";
+
+describe("Left Navigation", () => {
+  it("renders without exploding", () => {
+    render(<LeftNavigation pages={pagesMockData} currentUrl="/" />);
+
+    const navElement = screen.getByRole("navigation");
+
+    expect(navElement).toBeInTheDocument();
+  });
+
+  it("toggles classes correctly when opened or closed", () => {
+    keepMount(isLeftNavigationOpen);
+
+    const { rerender } = render(
+      <LeftNavigation pages={pagesMockData} currentUrl="/" />
+    );
+
+    const navElement = screen.getByRole("navigation");
+
+    const parentDivElement = navElement.parentElement?.parentElement;
+
+    expect(parentDivElement).not.toHaveClass("left-navigation--active");
+
+    act(() => {
+      isLeftNavigationOpen.set(true);
+      rerender(<LeftNavigation pages={pagesMockData} currentUrl="/" />);
+    });
+
+    expect(parentDivElement).toHaveClass("left-navigation--active");
+
+    act(() => {
+      isLeftNavigationOpen.set(false);
+      rerender(<LeftNavigation pages={pagesMockData} currentUrl="/" />);
+    });
+
+    expect(parentDivElement).toHaveClass("left-navigation--hidden");
+
+    cleanStores(isLeftNavigationOpen);
+  });
+
+  it("sets and traps focus correctly", async () => {
+    keepMount(isLeftNavigationOpen);
+
+    isLeftNavigationOpen.set(true);
+
+    render(<LeftNavigation pages={pagesMockData} currentUrl="/" />);
+
+    const button = screen.getByRole("button");
+    const firstLink = screen.getByText("Home");
+    const lastLink = screen.getByText("Contact");
+
+    expect(firstLink).toHaveFocus();
+
+    await userEvent.keyboard("{Tab}");
+    await userEvent.keyboard("{Tab}");
+    await userEvent.keyboard("{Tab}");
+
+    expect(button).toHaveFocus();
+
+    await userEvent.keyboard("{Shift>}{Tab}");
+
+    expect(lastLink).toHaveFocus();
+
+    cleanStores(isLeftNavigationOpen);
+  });
+
+  it("passes basic axe compliance", async () => {
+    const { container } = render(
+      <LeftNavigation pages={pagesMockData} currentUrl="/" />
+    );
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+});
