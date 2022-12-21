4fb32540f5819a59ca08dd95026dbb4d66995a94
Joe S <jsebast@avaya.com>
2022-12-21 12:55:58 -0800

    fix up examples

--- src/pageletes/components/icon-states/PlaygroundImplementation.tsx
+++ src/pageletes/components/icon-states/PlaygroundImplementation.tsx
@@ -1,6 +1,8 @@
 import {
   Icon,
   IconProps,
+  Radio,
+  RadioGroup,
   Select,
   SelectOption,
   TextInput,
@@ -8,6 +10,8 @@ import {
 import { useMemo, useState } from "react";
 
 import { Playground } from "components";
+import { prettyPrintHtml, prettyPrintReact } from "helpers";
+import clsx from "clsx";
 
 const defaultLabel = "Save Icon";
 const sandbox =
@@ -16,20 +20,34 @@ const storybook =
   "https://neo-react-library-storybook.netlify.app/?path=/story/components-icon";
 
 export const PlaygroundImplementation = () => {
-  const [icon, setIcon] = useState<IconProps["icon"]>("info");
+  const [icon, setIcon] = useState<IconProps["icon"]>("email");
   const [label, setLabel] = useState<IconProps["aria-label"]>(defaultLabel);
+  const [size, setSize] = useState<IconProps["size"]>("lg");
+  const [status, setStatus] = useState<IconProps["status"]>("available");
 
   const react = useMemo(
-    () => `<Icon icon="${icon}" aria-label="${label}" />`,
-    [icon, label]
+    () =>
+      prettyPrintReact(
+        `<Icon aria-label="${label}" icon="${icon}" size="${size}" status="${status}" />`
+      ),
+    [icon, label, size, status]
   );
 
   const html = useMemo(
     () =>
-      `<div class="neo-empty-state">
-  <p class="neo-icon-${icon}">${label}</p>
-</div>`,
-    [icon, label]
+      prettyPrintHtml(
+        `<span
+        role="img"
+        aria-label="${label}"
+        class="${clsx(
+          "neo-icon-state",
+          size === "lg" && "neo-icon-state--large",
+          `neo-icon-state--${status}`,
+          `neo-icon-${icon}`
+        )}"
+        ></span>`
+      ),
+    [icon, label, size, status]
   );
 
   return (
@@ -42,7 +60,7 @@ export const PlaygroundImplementation = () => {
               value={icon}
               onChange={(value) => setIcon(value as IconProps["icon"])}
             >
-              {["info", "warning", "error"].map((value) => (
+              {["menu", "table", "email"].map((value) => (
                 <SelectOption key={value} value={value}>
                   {value}
                 </SelectOption>
@@ -50,9 +68,37 @@ export const PlaygroundImplementation = () => {
             </Select>
           </Playground.OptionsSection>
 
-          <Playground.OptionsSection title="Text">
+          <Playground.OptionsSection title="Status">
+            <Select
+              aria-label="Status"
+              value={status}
+              onChange={(value) => setStatus(value as IconProps["status"])}
+            >
+              {["available", "away", "busy"].map((value) => (
+                <SelectOption key={value} value={value}>
+                  {value}
+                </SelectOption>
+              ))}
+            </Select>
+          </Playground.OptionsSection>
+
+          <Playground.OptionsSection title="Size">
+            <RadioGroup
+              groupName="size-options"
+              selected={size}
+              onChange={(e) => {
+                setSize(e.target.value as IconProps["size"]);
+              }}
+            >
+              <Radio value="sm">Small</Radio>
+              <Radio value="lg">Large</Radio>
+            </RadioGroup>
+          </Playground.OptionsSection>
+
+          <Playground.OptionsSection title="Aria Label">
             <TextInput
-              aria-label="Text"
+              aria-label="Aria Label"
+              defaultValue={label}
               onChange={(e) => setLabel(e.target.value || defaultLabel)}
             />
           </Playground.OptionsSection>
@@ -65,13 +111,7 @@ export const PlaygroundImplementation = () => {
         storybook,
       }}
     >
-      <Icon
-        aria-label="Testing size xs"
-        className="btn-icon-test another-class"
-        icon="save"
-        size="sm"
-        status="busy"
-      />
+      <Icon aria-label={label} icon={icon} size={size} status={status} />
     </Playground>
   );
 };
