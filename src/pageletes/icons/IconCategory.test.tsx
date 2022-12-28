26151e432ae7fb3924cd25638264082927dad98c
mrazian85 <61242168+mrazian85@users.noreply.github.com>
2022-12-28 13:27:57 -0500

    Adding tests for IconLibrary, IconCategory and IconFilters Components (#125)
    * Added tests for IconLibrary, IconCategory and IconFilters Components; formatting
    
    * Clearing store after IconLibrary test

---
+++ src/pageletes/icons/IconCategory.test.tsx
@@ -0,0 +1,77 @@
+import { act, render, screen } from "@testing-library/react";
+import { axe } from "jest-axe";
+import { cleanStores, keepMount } from "nanostores";
+
+import { variationsToFilterFor } from "./helpers/iconPageState";
+import { icons } from "./helpers/icons";
+import { IconCategory } from "./IconCategory";
+
+describe("IconCategory", () => {
+  const exampleCategory = "content";
+
+  it("renders without exploding", () => {
+    render(<IconCategory category={exampleCategory} />);
+
+    const iconCategoryElement = screen.getByText(exampleCategory);
+
+    expect(iconCategoryElement).toBeInTheDocument();
+  });
+
+  it("displays error message when no icons are displayed", () => {
+    render(<IconCategory category="" />);
+
+    const errorMessage = screen.getByText(
+      "No icons to display with current filters"
+    );
+
+    expect(errorMessage).toBeInTheDocument();
+  });
+
+  it("responds correctly when variants are filtered for", () => {
+    keepMount(variationsToFilterFor);
+
+    render(<IconCategory category={exampleCategory} />);
+
+    const allIconsInCategory = icons.filter(
+      (icon) => icon.category === exampleCategory
+    );
+
+    const bidirectionalIconsInCategory = allIconsInCategory.filter(
+      (icon) => icon.bidirectional
+    );
+
+    const nonBidirectionalIconsInCategory = allIconsInCategory.filter(
+      (icon) => !icon.bidirectional
+    );
+
+    allIconsInCategory.forEach((icon) => {
+      const renderedIcon = screen.queryByText(icon.name);
+
+      expect(renderedIcon).toBeInTheDocument();
+    });
+
+    act(() => {
+      variationsToFilterFor.set(["bidirectional"]);
+    });
+
+    bidirectionalIconsInCategory.forEach((icon) => {
+      const renderedIcon = screen.queryByText(icon.name);
+
+      expect(renderedIcon).toBeInTheDocument();
+    });
+
+    nonBidirectionalIconsInCategory.forEach((icon) => {
+      const renderedIcon = screen.queryByText(icon.name);
+
+      expect(renderedIcon).not.toBeInTheDocument();
+    });
+
+    cleanStores(variationsToFilterFor);
+  });
+
+  it("passes basic axe compliance", async () => {
+    const { container } = render(<IconCategory category={exampleCategory} />);
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+});
---
+++ src/pageletes/icons/IconLibrary.test.tsx
@@ -0,0 +1,53 @@
+import { act, render, screen } from "@testing-library/react";
+import { axe } from "jest-axe";
+import { cleanStores, keepMount } from "nanostores";
+
+import { categoriesToFilterFor } from "./helpers/iconPageState";
+import { iconCategories, icons } from "./helpers/icons";
+import { IconLibrary } from "./IconLibrary";
+
+describe("IconLibrary", () => {
+  it("renders without exploding", () => {
+    render(<IconLibrary allCategories={iconCategories} />);
+
+    const iconLibraryChipElement = screen.getByRole("alert");
+
+    expect(iconLibraryChipElement).toBeInTheDocument();
+  });
+
+  it("updates number of icons displayed in chip based on filters", () => {
+    keepMount(categoriesToFilterFor);
+
+    render(<IconLibrary allCategories={iconCategories} />);
+
+    const allIconsLength = icons.length;
+
+    const iconLibraryChipElement = screen.getByRole("alert");
+
+    expect(iconLibraryChipElement).toHaveTextContent(
+      `${allIconsLength} icons displayed`
+    );
+
+    act(() => {
+      categoriesToFilterFor.set(["navigation"]);
+    });
+
+    const iconsInCategoryLength = icons.filter(
+      (icon) => icon.category === "navigation"
+    ).length;
+
+    expect(iconLibraryChipElement).toHaveTextContent(
+      `${iconsInCategoryLength} icons displayed`
+    );
+
+    cleanStores(categoriesToFilterFor)
+  });
+
+  it("passes basic axe compliance", async () => {
+    const { container } = render(
+      <IconLibrary allCategories={iconCategories} />
+    );
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+});
