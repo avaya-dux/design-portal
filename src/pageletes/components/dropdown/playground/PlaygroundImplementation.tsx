90b38de2313716233ffa7faf53c77f0f66f4618f
mrazian85 <mrazian@avaya.com>
2023-01-05 12:19:36 -0500

    Removed unnecessary className prop

--- src/pageletes/components/dropdown/playground/PlaygroundImplementation.tsx
+++ src/pageletes/components/dropdown/playground/PlaygroundImplementation.tsx
@@ -117,7 +117,6 @@ export const PlaygroundImplementation = () => {
       }}
     >
       <Menu
-        className="neo-dropdown--active"
         menuRootElement={<Button>Action</Button>}
         closeOnSelect={false}
       >
