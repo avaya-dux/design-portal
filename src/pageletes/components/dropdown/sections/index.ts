e67059064a964e07a89f44cbc2ed96cc13c933bd
mrazian85 <61242168+mrazian85@users.noreply.github.com>
2023-01-09 15:24:39 -0500

    NEO-1369: Dropdown page Part 3 (#135)
    * Began building Dropdown Component page
    
    * Added Anatomy section, continued tweaking Dropdown playground
    
    * Tweaked Dropdown example and code
    
    * Added conditional logic to React code example in Playground
    
    * Added Anatomy section to page, added link to Sandbox
    
    * Removed unnecessary className prop
    
    * Pick up feedback on PR
    
    * Began building out other Dropdown page sections
    
    * Made TextInput self-closing
    
    * Continue building out Dropdown page sections
    
    * Continued building out Dropdown sections
    
    * Picked up further feedback from PR
    
    * Divided Positioning section into several sub-components, continued building rest of page content
    
    * Remove Types component for addition in subsequent PR
    
    * Fixed issue with conditional render statement
    
    * Ran yarn all
    
    * Continued adding content to Dropdown page
    
    * Fixed issue with faulty image import
    
    * Updated States image
    
    * Formatted image imports for consistency
    
    * Built out remaining sections of Dropdown page, corrected all faulty images being imported
    
    * Added TO-DO comments for all URLs that will need to be added later

--- src/pageletes/components/dropdown/sections/index.ts
+++ src/pageletes/components/dropdown/sections/index.ts
@@ -17,3 +17,23 @@ export { States };
 import Positioning from "./Positioning.astro";
 
 export { Positioning };
+
+import Types from "./Types.astro";
+
+export { Types };
+
+import Behavior from "./Behavior.astro";
+
+export { Behavior };
+
+import Specs from "./Specs.astro";
+
+export { Specs };
+
+import KeyboardInteractions from "./KeyboardInteractions.astro";
+
+export { KeyboardInteractions };
+
+import RelatedContent from "./RelatedContent.astro";
+
+export { RelatedContent };
