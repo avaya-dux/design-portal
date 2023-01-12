9aed39cbd36a8790bd2df1928033654a9d5026e3
Joe S <jsebast@avaya.com>
2023-01-12 13:24:55 -0600

    add Behavior sections

---
+++ src/pageletes/components/tooltip/BehaviorDoAndDont/index.ts
@@ -0,0 +1,20 @@
+import Placement from "./Placement.astro";
+export { Placement };
+
+import Instructions from './Instructions.astro';
+export { Instructions };
+
+import HelperText from './HelperText.astro';
+export { HelperText };
+
+import ContentRules from './ContentRules.astro';
+export { ContentRules };
+
+import Wording from './Wording.astro';
+export { Wording };
+
+import Positioning from './Positioning.astro';
+export { Positioning };
+
+import Multiline from './Multiline.astro';
+export { Multiline };
