efc6495226c5f338e77a0f400089a848fad657f0
Joe S <jsebast@avaya.com>
2023-01-18 12:49:07 -0600

    more tests
    mostly written via copilot

--- src/pageletes/components/tooltip/Playground/helpers.test.ts
+++ src/pageletes/components/tooltip/Playground/helpers.test.ts
@@ -1,4 +1,8 @@
-import { shouldDisableOffset } from "./helpers";
+import type { TooltipPosition } from "@avaya/neo-react";
+import {
+  shouldDisableOffset,
+  translatePositionToCSSName,
+} from "./helpers";
 
 describe("shouldDisableOffset", () => {
   it("should return true for left and right", () => {
@@ -10,5 +14,41 @@ describe("shouldDisableOffset", () => {
   it("should return false for top and bottom", () => {
     expect(shouldDisableOffset("top")).toBe(false);
     expect(shouldDisableOffset("bottom")).toBe(false);
+  });
+});
+
+describe("translatePositionToCSSName", () => {
+  it("should return 'up' if passed 'top' or 'auto", () => {
+    expect(translatePositionToCSSName("top")).toBe("up");
+    expect(translatePositionToCSSName("auto")).toBe("up");
+  });
+
+  it("should return matching string if passed 'left' or 'right'", () => {
+    expect(translatePositionToCSSName("left")).toBe("left");
+    expect(translatePositionToCSSName("right")).toBe("right");
+  });
+
+  it("should return 'down' if passed 'bottom'", () => {
+    expect(translatePositionToCSSName("bottom")).toBe("down");
+  });
+
+  it("should return 'up-left' if passed 'top-left'", () => {
+    expect(translatePositionToCSSName("top-left")).toBe("up-left");
+  });
+
+  it("should return 'up-right' if passed 'top-right'", () => {
+    expect(translatePositionToCSSName("top-right")).toBe("up-right");
+  });
+
+  it("should return 'down-left' if passed 'bottom-left'", () => {
+    expect(translatePositionToCSSName("bottom-left")).toBe("down-left");
+  });
+
+  it("should return 'down-right' if passed 'bottom-right'", () => {
+    expect(translatePositionToCSSName("bottom-right")).toBe("down-right");
+  });
+
+  it("should return 'up' if passed a bad value", () => {
+    expect(translatePositionToCSSName("bad" as TooltipPosition)).toBe("up");
   });
 });
