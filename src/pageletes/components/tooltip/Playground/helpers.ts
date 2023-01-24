87f559c348df727d1f7932e7d2b425b978764fc8
JoeS <55896546+joe-s-avaya@users.noreply.github.com>
2023-01-24 09:18:21 -0800

    NEO-1376 // add Tooltip Playground (temp images) (#144)
    * Tooltip page // add most of the logic
    
    * convert Select's to Radio Groups
    
    * simplify
    
    * use `multiline` prop
    
    * move helper functions into their own file and add initial test
    
    moving around all Tooltip Playground files to keep things clean
    
    * more tests
    
    mostly written via copilot
    
    * man do I love copilot
    
    * update badges
    
    * fix typo
    
    * upper case first letter of string
    
    * fix typo
    
    * remove "offset" logic
    
    per Matt's request so as to simplify logic
    
    * remove defunct tests
    
    * force tooltip width in specific conditions
    
    * cleanup tests
    
    * update badges
    
    * remove "type" as an option, auto update it based on selected position
    
    * `yarn format`
    
    Co-authored-by: Joe S <jsebast@avaya.com>

---
+++ src/pageletes/components/tooltip/Playground/helpers.ts
@@ -0,0 +1,54 @@
+import type { TooltipPosition } from "@avaya/neo-react";
+
+// (mostly) copy-pasted from Tooltip/helpers.ts
+export const translatePositionToCSSName = (
+  position: TooltipPosition = "top"
+): string => {
+  let cssPosition;
+
+  switch (position) {
+    case "left":
+      cssPosition = "left";
+      break;
+
+    case "right":
+      cssPosition = "right";
+      break;
+
+    case "bottom":
+      cssPosition = "down";
+      break;
+
+    case "top":
+    case "auto":
+      cssPosition = "up";
+      break;
+
+    case "top-left":
+      cssPosition = "up-left";
+      break;
+
+    case "top-right":
+      cssPosition = "up-right";
+      break;
+
+    case "bottom-left":
+      cssPosition = "down-left";
+      break;
+
+    case "bottom-right":
+      cssPosition = "down-right";
+      break;
+
+    default:
+      console.error(
+        `Unexpected position encountered: ${position}. Defaulting to default "position='up'"`
+      );
+      cssPosition = "up";
+  }
+
+  return `neo-tooltip--${cssPosition}`;
+};
+
+export const upperCaseFirstLetter = (string: string) =>
+  string.charAt(0).toUpperCase() + string.slice(1);
