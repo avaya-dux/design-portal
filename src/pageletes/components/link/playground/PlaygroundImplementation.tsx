8729880cbbd2be8785960793928492a1bbcf9a45
Chunguang Yang <chungguangy@avaya.com>
2023-01-30 15:44:39 -0500

    update storybook link

--- src/pageletes/components/link/playground/PlaygroundImplementation.tsx
+++ src/pageletes/components/link/playground/PlaygroundImplementation.tsx
@@ -20,7 +20,7 @@ type IconPlacement = "none" | "left" | "right";
 import "./PlaygroundImplementation.css";
 
 const storybook =
-  "https://neo-react-library-storybook.netlify.app/?path=/story/components-link--default";
+  "https://neo-react-library-storybook.netlify.app/?path=/story/components-link";
 const sandbox = "https://codesandbox.io/s/neo-react-link-pr4mw7";
 
 export const PlaygroundImplementation = () => {
