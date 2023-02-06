9fc349a797ea0e4911025d9bb954aeec7945c354
Chunguang Yang <chungguangy@avaya.com>
2023-02-06 14:34:05 -0500

    remove console.log

--- src/components/react/DynamicTheme.tsx
+++ src/components/react/DynamicTheme.tsx
@@ -3,7 +3,6 @@ import { themesToFilterFor } from "./utils";
 
 export const DynamicTheme = ({ children }: { children: React.ReactNode }) => {
   const filteredTheme = useStore(themesToFilterFor);
-  console.log({ filteredTheme });
   return (
     <div className={`neo-${filteredTheme} main__theme__styles`}>{children}</div>
   );
