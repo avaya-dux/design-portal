e5838a53c69c50214fd6d9923a1a98c5d2ab48f7
Joe S <jsebast@avaya.com>
2023-02-15 13:22:59 -0600

    update `static.ts`

--- src/pageletes/components/spinner/static.ts
+++ src/pageletes/components/spinner/static.ts
@@ -6,12 +6,37 @@ export const sandbox = "https://codesandbox.io/s/neo-react-spinner-yj5o5i";
 export const storybook =
   "https://neo-react-library-storybook.netlify.app/?path=/story/components-spinner--default";
 
-type SpinnerTopicNames = "playground";
+type SpinnerTopicNames =
+  | "playground"
+  | "anatomy"
+  | "behavior"
+  | "specs"
+  | "related";
 
 export const topics: TopicsDict<SpinnerTopicNames> = {
   playground: {
     order: 0,
     id: "spinner-playground",
     title: "Interactive Playground",
+  },
+  anatomy: {
+    order: 1,
+    id: "anatomy",
+    title: "Anatomy",
+  },
+  behavior: {
+    order: 2,
+    id: "behavior",
+    title: "Behavior",
+  },
+  specs: {
+    order: 3,
+    id: "specs",
+    title: "Specs",
+  },
+  related: {
+    order: 4,
+    id: "related",
+    title: "Related Content",
   },
 };
