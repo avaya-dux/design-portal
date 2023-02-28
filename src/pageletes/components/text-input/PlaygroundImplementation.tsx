4ca3e1361ef5e3dc9278898ed49238cb9d999bee
Enrique Prado <enriquep@avaya.com>
2023-02-28 12:55:17 -0800

    Final CR fixes

--- src/pageletes/components/text-input/PlaygroundImplementation.tsx
+++ src/pageletes/components/text-input/PlaygroundImplementation.tsx
@@ -1,7 +1,7 @@
 import type { TextInputProps } from "@avaya/neo-react";
 import { Radio, RadioGroup, TextInput } from "@avaya/neo-react";
 import { useMemo, useState } from "react";
-import { prettyPrintHtml, prettyPrintReact } from "helpers";
+import { prettyPrintReact } from "helpers";
 
 import { Playground } from "components";
 
@@ -14,17 +14,17 @@ type LabelOption = "optional" | "required" | "none";
 export const PlaygroundImplementation = () => {
   const [labelOption, setLabelOption] = useState<LabelOption>("optional");
   const [required, setRequired] = useState<TextInputProps["required"]>(false);
+  const [inputValue, setInputValue] = useState<TextInputProps["value"]>("");
   const [helperText, setHelperText] =
     useState<TextInputProps["helperText"]>("Helper text");
   const [error, setError] = useState<TextInputProps["error"]>(false);
   const [typeOption, setTypeOption] = useState<TypeOption>("text");
 
   const [html, react] = useMemo(() => {
-    const htmlCode = prettyPrintHtml(
-      `
+    const htmlCode = `
 <div class="neo-form-control" ${clsx(
-        error && " neo-form-control--error  neo-form-control--required"
-      )}>
+      error && " neo-form-control--error  neo-form-control--required"
+    )}>
   <div class="neo-input-group">
     <label for="input1">
       Name
@@ -35,26 +35,31 @@ export const PlaygroundImplementation = () => {
         id="input1"
         aria-describedby="text-hint"
         type="${typeOption}"
-      />
+    />
       <button aria-label="clear input" class="neo-input-edit__icon neo-icon-end"></button>
+      ${clsx(
+        typeOption === "password" &&
+          '<button aria-label="Show Password" aria-pressed="false" class="neo-icon-view-on" type="button"></button>'
+      )}
     </div>
   </div>
   <div id="text-hint" class="neo-input-hint" ${clsx(
     error && ' aria-live="assertive"'
   )}>${helperText}</div>
 </div>
-`
-    );
+`;
 
     const reactCode = prettyPrintReact(
       `<TextInput
   type="${typeOption}"
-  label="Label" helperText="${helperText}"
+  label="Label"
+  helperText="${helperText}"
   required="${required}"
-  error="${error}"/>`
+  error="${error}"
+/>`
     );
     return [htmlCode, reactCode];
-  }, [helperText, typeOption]);
+  }, [error, helperText, required, typeOption]);
 
   return (
     <Playground
@@ -85,6 +90,7 @@ export const PlaygroundImplementation = () => {
                   setError(false);
                   setHelperText("Helper text");
                 } else {
+                  setInputValue("");
                   setRequired(true);
                   setError(true);
                   setHelperText("This field is required");
@@ -98,18 +104,23 @@ export const PlaygroundImplementation = () => {
         </Playground.OptionsContainer>
       }
       examples={{
-        html: html,
-        react: react,
+        html,
+        react,
         sandbox,
         storybook,
       }}
     >
       <TextInput
         label="Label"
+        value={inputValue}
         type={typeOption}
         helperText={helperText}
         required={required}
         error={error}
+        onChange={(e) => {
+          setInputValue(e.target.value);
+          setError(false);
+        }}
       />
     </Playground>
   );
