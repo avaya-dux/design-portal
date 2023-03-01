990a6bb36228cf9c7775f5c35d92249371260874
Joe S <jsebast@avaya.com>
2023-03-01 15:53:30 -0600

    add Type to Select page

---
+++ src/pageletes/components/select/sections/Type/SubSections/index.ts
@@ -0,0 +1,11 @@
+import Intro from "./Intro.astro";
+export { Intro };
+
+import Single from "./Single.astro";
+export { Single };
+
+import Multiple from "./Multiple.astro";
+export { Multiple };
+
+import Combobox from "./Combobox.astro";
+export { Combobox };
