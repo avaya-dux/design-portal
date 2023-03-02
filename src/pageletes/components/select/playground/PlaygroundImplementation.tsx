4132e8d9bc7e733dc094ae7123897f9177443d86
Joe S <jsebast@avaya.com>
2023-03-02 14:02:27 -0600

    remove copy-pasted code

--- src/pageletes/components/select/playground/PlaygroundImplementation.tsx
+++ src/pageletes/components/select/playground/PlaygroundImplementation.tsx
@@ -1,182 +1,22 @@
-import { Form, Radio, RadioGroup } from "@avaya/neo-react";
-import { useMemo, useState } from "react";
-
-import clsx from "clsx";
+import { useMemo } from "react";
 
 import { Playground } from "components/react";
-import { prettyPrintHtml, prettyPrintReact } from "helpers";
 
-const sandbox = "https://codesandbox.io/s/neo-react-radio-group-d5l4uz";
+const sandbox = "https://codesandbox.io/";
 const storybook =
-  "https://neo-react-library-storybook.netlify.app/?path=/story/components-radio-group";
-
-const defaultHtml = prettyPrintHtml(`
-<form class="neo-form">
-  <div class="neo-form-control">
-    <div class="neo-input-group">
-      <label for="Default Radio Group">Select Option</label>
-      <input
-        class="neo-radio"
-        type="radio"
-        name="Default Radio Group"
-        value="Calendar"
-        id="calendar"
-        role="radio"
-        aria-checked="false"
-      />
-      <label for="calendar">Calendar</label>
-      <input
-        class="neo-radio"
-        type="radio"
-        name="Default Radio Group"
-        value="Calculator"
-        id="calculator"
-        role="radio"
-        aria-checked="false"
-      />
-      <label for="calculator">Calculator</label>
-      <input
-        class="neo-radio"
-        type="radio"
-        name="Default Radio Group"
-        value="Notes"
-        id="notes"
-        role="radio"
-        aria-checked="false"
-      />
-      <label for="notes">Notes</label>
-    </div>
-  </div>
-</form>
-  `);
-
-const defaultReact = prettyPrintReact(`
-<Form id="radio-form">
-  <RadioGroup
-    groupName="Default Radio Group"
-    label="Select Option"
-    selected={selectedExampleRadio}
-    onChange={(e) => setSelectedExampleRadio(e.target.value)}
-  >
-    <Radio value="Calendar">Calendar</Radio>
-    <Radio value="Calculator">Calculator</Radio>
-    <Radio value="Notes">Notes</Radio>
-  </RadioGroup>
-</Form>
-  `);
+  "https://neo-react-library-storybook.netlify.app/?path=/story/components-select--basic-selects";
 
 export const PlaygroundImplementation = () => {
-  const [withLabel, setWithLabel] = useState<string>("withLabel");
-
-  const [isVertical, setIsVertical] = useState<string>("isVertical");
-
-  const [selectedExampleRadio, setSelectedExampleRadio] = useState<string>("");
-
   const [react, html] = useMemo(() => {
-    const isDefault =
-      withLabel === "withLabel" &&
-      isVertical === "isVertical" &&
-      selectedExampleRadio === "";
-
-    const labelAttr =
-      withLabel === "withLabel" ? "label='Select Option'" : undefined;
-
-    const inlineAttr = isVertical === "isNotVertical" ? "inline" : undefined;
-
-    const reactCode = prettyPrintReact(
-      `
-<Form id="radio-form">
-  <RadioGroup
-    ${clsx("groupName='Default Radio Group'", labelAttr, inlineAttr)}
-    selected={selectedExampleRadio}
-    onChange={(e) => setSelectedExampleRadio(e.target.value)}
-  >
-    <Radio value="Calendar">Calendar</Radio>
-    <Radio value="Calculator">Calculator</Radio>
-    <Radio value="Notes">Notes</Radio>
-  </RadioGroup>
-</Form>
-`
-    );
-
-    const htmlCode = prettyPrintHtml(
-      `
-      <form class="neo-form">
-  <div class="neo-form-control">
-    <div class="neo-input-group${
-      isVertical === "isNotVertical" ? "neo-input-group--inline" : ""
-    }">
-      ${
-        withLabel === "withLabel"
-          ? '<label for="Default Radio Group">Select Option</label>'
-          : ""
-      }
-      <input
-        class="neo-radio"
-        type="radio"
-        name="Default Radio Group"
-        value="Calendar"
-        id="calendar"
-        role="radio"
-        aria-checked=${selectedExampleRadio === "Calendar"}
-      />
-      <label for="calendar">Calendar</label>
-      <input
-        class="neo-radio"
-        type="radio"
-        name="Default Radio Group"
-        value="Calculator"
-        id="calculator"
-        role="radio"
-        aria-checked=${selectedExampleRadio === "Calculator"}
-      />
-      <label for="calculator">Calculator</label>
-      <input
-        class="neo-radio"
-        type="radio"
-        name="Default Radio Group"
-        value="Notes"
-        id="notes"
-        role="radio"
-        aria-checked=${selectedExampleRadio === "Notes"}
-      />
-      <label for="notes">Notes</label>
-      </div>
-  </div>
-</form>
-`
-    );
-
-    return isDefault ? [defaultReact, defaultHtml] : [reactCode, htmlCode];
-  }, [isVertical, withLabel, selectedExampleRadio]);
+    return ["reactCode", "htmlCode"];
+  }, []);
 
   return (
     <Playground
       options={
         <Playground.OptionsContainer>
-          <Playground.OptionsSection title="Label">
-            <RadioGroup
-              groupName="label"
-              selected={withLabel}
-              onChange={(e) => {
-                setWithLabel(e.target.value);
-              }}
-            >
-              <Radio value="withLabel">With Label</Radio>
-              <Radio value="withoutLabel">Without Label</Radio>
-            </RadioGroup>
-          </Playground.OptionsSection>
-          <Playground.OptionsSection title="Orientation">
-            <RadioGroup
-              groupName="options"
-              selected={isVertical}
-              onChange={(e) => {
-                setIsVertical(e.target.value);
-              }}
-            >
-              <Radio value="isVertical">Vertical</Radio>
-              <Radio value="isNotVertical">Horizontal</Radio>
-            </RadioGroup>
+          <Playground.OptionsSection title="States">
+            <p>TODO: add options</p>
           </Playground.OptionsSection>
         </Playground.OptionsContainer>
       }
@@ -187,19 +27,7 @@ export const PlaygroundImplementation = () => {
         storybook,
       }}
     >
-      <Form id="radio-form">
-        <RadioGroup
-          groupName="Select Option"
-          label={withLabel === "withLabel"}
-          inline={isVertical === "isNotVertical"}
-          selected={selectedExampleRadio}
-          onChange={(e) => setSelectedExampleRadio(e.target.value)}
-        >
-          <Radio value="Calendar">Calendar</Radio>
-          <Radio value="Calculator">Calculator</Radio>
-          <Radio value="Notes">Notes</Radio>
-        </RadioGroup>
-      </Form>
+      <p>TODO: add Select</p>
     </Playground>
   );
 };
