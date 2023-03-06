ee1e849d6849d547b017ce8be70c588b49aa06ef
mrazian85 <mrazian@avaya.com>
2023-03-06 13:20:56 -0500

    Updated to React library 0.8.1, added Interactive Playground implementation to icon page

--- src/pageletes/components/icon/sections/index.ts
+++ src/pageletes/components/icon/sections/index.ts
@@ -4,6 +4,10 @@ export { Intro };
 
 import Anatomy from "./Anatomy.astro";
 
+import Playground from "./IconPlayground.astro";
+
+export { Playground }
+
 export { Anatomy };
 
 import Behavior from "./Behavior.astro";
