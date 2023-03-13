4099c4ff72df57a4b54c344cf189d4d13b36f98a
Chunguang Yang <chungguangy@avaya.com>
2023-03-13 09:54:35 -0400

    add keyboard and related sections

--- src/pageletes/components/checkbox/playground/PlaygroundImplementation.tsx
+++ src/pageletes/components/checkbox/playground/PlaygroundImplementation.tsx
@@ -1,6 +1,5 @@
-
 const codeSandboxLink = "https://codesandbox.io/s/neo-react-checkbox-kb5gbt";
 
 export const PlaygroundImplementation = () => {
-    return <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At repudiandae saepe nam quas debitis tenetur vitae totam quibusdam alias, molestiae repellendus perspiciatis dolores sunt illum eaque veritatis iste tempore modi?</div>
-}
+  return <>placeholder</>;
+};
