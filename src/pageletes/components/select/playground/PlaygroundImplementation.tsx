7714c9ac1992e31d1bfddcba84d31e15762affbd
JoeS <55896546+joe-s-avaya@users.noreply.github.com>
2023-03-14 14:37:51 -0700

    Add Select Playground (no code examples yet) (#193)
    * add most of the Select Playground functionality
    
    no example content yet
    
    * update sandbox URL, fix typo
    
    * add `size` prop
    
    * whoops
    
    * update type don't 2 image
    
    * Per Matt's instructions, update Info Table header styles
    
    * improve comment
    
    * per Matt's request, info table fix border colors
    
    ---------
    
    Co-authored-by: Joe S <jsebast@avaya.com>

--- src/pageletes/components/select/playground/PlaygroundImplementation.tsx
+++ src/pageletes/components/select/playground/PlaygroundImplementation.tsx
@@ -1,12 +1,55 @@
-import { useMemo } from "react";
+import type { SelectProps } from "@avaya/neo-react";
+import {
+  Checkbox,
+  CheckboxGroup,
+  Radio,
+  RadioGroup,
+  Select,
+  SelectOption,
+} from "@avaya/neo-react";
+import { useMemo, useState } from "react";
 
 import { Playground } from "components/react";
 
-const sandbox = "https://codesandbox.io/";
+const sandbox = "https://codesandbox.io/s/neo-react-select-05kbd5";
 const storybook =
   "https://neo-react-library-storybook.netlify.app/?path=/story/components-select--basic-selects";
 
+const fruitOptions = [
+  <SelectOption value="apple" key="apple">
+    Apple
+  </SelectOption>,
+  <SelectOption value="gravel" key="gravel" helperText="Not a Fruit" disabled>
+    Gravel
+  </SelectOption>,
+  <SelectOption value="broccoli" key="broccoli" helperText="Vegetable">
+    Broccoli
+  </SelectOption>,
+  <SelectOption value="banana" key="banana">
+    Banana
+  </SelectOption>,
+  <SelectOption value="pear" key="pear">
+    Pear
+  </SelectOption>,
+  <SelectOption value="blueberries" key="blueberries">
+    Blueberries
+  </SelectOption>,
+  <SelectOption value="grapes" key="grapes">
+    Grapes
+  </SelectOption>,
+  <SelectOption value="oranges" key="oranges">
+    Oranges
+  </SelectOption>,
+];
+
 export const PlaygroundImplementation = () => {
+  const [multiple, setMultiple] = useState(false);
+  const [hasError, setHasError] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [hasVisibleLabel, setHasVisibleLabel] = useState(true);
+  const [hasHelperText, setHasHelperText] = useState(true);
+  const [size, setSize] = useState<SelectProps["size"]>("md");
+
   const [react, html] = useMemo(() => {
     return ["reactCode", "htmlCode"];
   }, []);
@@ -15,8 +58,76 @@ export const PlaygroundImplementation = () => {
     <Playground
       options={
         <Playground.OptionsContainer>
-          <Playground.OptionsSection title="States">
-            <p>TODO: add options</p>
+          <Playground.OptionsSection title="Type">
+            <Select
+              aria-label="Type of Select"
+              defaultValue="single"
+              onChange={(value) => {
+                setMultiple(value === "multiple");
+              }}
+            >
+              <SelectOption value="single">Single</SelectOption>
+              <SelectOption value="multiple">Multiple</SelectOption>
+            </Select>
+          </Playground.OptionsSection>
+
+          <Playground.OptionsSection
+            id="select-state-options-title"
+            title="States"
+          >
+            <CheckboxGroup
+              groupName="select-state-options"
+              aria-labelledby="select-state-options-title"
+            >
+              <Checkbox value="error" onChange={() => setHasError(!hasError)}>
+                Error
+              </Checkbox>
+              <Checkbox
+                value="disabled"
+                onChange={() => setIsDisabled(!isDisabled)}
+              >
+                Disabled
+              </Checkbox>
+            </CheckboxGroup>
+          </Playground.OptionsSection>
+
+          <Playground.OptionsSection
+            id="select-state-variables-title"
+            title="Variables"
+          >
+            <CheckboxGroup
+              groupName="select-state-variables"
+              aria-labelledby="select-state-variables-title"
+            >
+              <Checkbox
+                value="label"
+                checked={hasVisibleLabel}
+                onChange={() => setHasVisibleLabel(!hasVisibleLabel)}
+              >
+                Label
+              </Checkbox>
+              <Checkbox
+                value="helper-text"
+                checked={hasHelperText}
+                onChange={() => setHasHelperText(!hasHelperText)}
+              >
+                Helper Text
+              </Checkbox>
+            </CheckboxGroup>
+          </Playground.OptionsSection>
+
+          <Playground.OptionsSection title="Size">
+            <RadioGroup
+              groupName="select-size"
+              selected={size}
+              onChange={(e) => {
+                const updatedSize = e.target.value as SelectProps["size"];
+                setSize(updatedSize);
+              }}
+            >
+              <Radio value="sm">Small</Radio>
+              <Radio value="md">Medium</Radio>
+            </RadioGroup>
           </Playground.OptionsSection>
         </Playground.OptionsContainer>
       }
@@ -27,7 +138,19 @@ export const PlaygroundImplementation = () => {
         storybook,
       }}
     >
-      <p>TODO: add Select</p>
+      <div style={{ width: "100%", maxWidth: "300px" }}>
+        <Select
+          aria-label={!hasVisibleLabel ? "Select a favorite food" : ""}
+          disabled={isDisabled}
+          errorList={hasError ? ["Invalid selection"] : []}
+          helperText={hasHelperText ? "Please select one" : undefined}
+          label={hasVisibleLabel ? "Select a favorite food" : undefined}
+          multiple={multiple}
+          size={size}
+        >
+          {fruitOptions}
+        </Select>
+      </div>
     </Playground>
   );
 };
