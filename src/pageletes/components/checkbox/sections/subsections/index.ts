37ba1bb4d18982234812c1baba71d1d6b46c1284
Chunguang Yang <chungguangy@avaya.com>
2023-03-17 16:31:48 -0400

    added anatomy, states, some behavior

---
+++ src/pageletes/components/checkbox/sections/subsections/index.ts
@@ -0,0 +1,17 @@
+import WhenToUse from "./When-to-use.astro";
+export { WhenToUse };
+
+import CategoryLabels from "./Category-labels.astro";
+export { CategoryLabels };
+
+import ContentOverflow from "./Content-overflow.astro";
+export { ContentOverflow };
+
+import TextAlignment from "./Text-alignment.astro";
+export { TextAlignment };
+
+import Indeterminate from "./Indeterminate.astro";
+export { Indeterminate };
+
+import Nesting from "./Nesting.astro";
+export { Nesting };
