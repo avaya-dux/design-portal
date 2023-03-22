fe407f95e8789e93d1311684e258d46a9afe57cc
Joe S <jsebast@avaya.com>
2023-03-22 16:23:40 -0500

    add missing index file

---
+++ src/pageletes/components/table/sections/index.ts
@@ -0,0 +1,26 @@
+import Intro from "./Intro.astro";
+export { Intro };
+
+import Playground from "./Playground.astro";
+export { Playground };
+
+// import Anatomy from "./Anatomy.astro";
+// export { Anatomy };
+
+// import States from "./States.astro";
+// export { States };
+
+// import Positioning from "./Positioning.astro";
+// export { Positioning };
+
+// import Behavior from "./Behavior.astro";
+// export { Behavior };
+
+import Specs from "./Specs.astro";
+export { Specs };
+
+import KeyboardInteractions from "./KeyboardInteractions.astro";
+export { KeyboardInteractions };
+
+import RelatedContent from "./RelatedContent.astro";
+export { RelatedContent };
