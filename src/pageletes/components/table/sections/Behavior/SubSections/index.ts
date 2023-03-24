cfef773174ab968f7e438ec845ff228e41be52f2
Joe S <jsebast@avaya.com>
2023-03-24 13:17:48 -0500

    add Selection section

--- src/pageletes/components/table/sections/Behavior/SubSections/index.ts
+++ src/pageletes/components/table/sections/Behavior/SubSections/index.ts
@@ -4,5 +4,8 @@ export { Intro };
 import RowHeight from "./RowHeight.astro";
 export { RowHeight };
 
+import Selection from "./Selection.astro";
+export { Selection };
+
 import Sorting from "./Sorting.astro";
 export { Sorting };
