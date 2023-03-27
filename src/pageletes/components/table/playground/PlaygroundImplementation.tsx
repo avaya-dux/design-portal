554f71904ff49b26968b931e75f40d2fd9448db3
Joe S <jsebast@avaya.com>
2023-03-27 09:45:41 -0500

    add basic table implementation to Playground

--- src/pageletes/components/table/playground/PlaygroundImplementation.tsx
+++ src/pageletes/components/table/playground/PlaygroundImplementation.tsx
@@ -1,25 +1,57 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 
 import { Playground } from "components/react";
 import {
   prettyPrintReactElementToHtml,
   prettyPrintReactElementToString,
 } from "helpers";
+import { Table } from "@avaya/neo-react";
+
+import styles from "./PlaygroundImplementation.module.css";
 
 const sandbox = "https://codesandbox.io/s/neo-react-table-p3yczb";
 const storybook =
   "https://neo-react-library-storybook.netlify.app/?path=/story/components-table";
 
+const columns = [
+  {
+    Header: "name header",
+    accessor: "name",
+  },
+  {
+    Header: "other header",
+    accessor: "other",
+  },
+];
+
 export const PlaygroundImplementation = () => {
+  const [data, setData] = useState([
+    { id: 1, name: "sir Fred", other: "Lorem Ipsum" },
+    { id: 2, name: "sir Daniel", other: "Lorem Ipsum" },
+    { id: 3, name: "madam Tif", other: "Lorem Ipsum" },
+    { id: 4, name: "madam Hailey", other: "Lorem Ipsum" },
+    { id: 5, name: "intersex Alex", other: "Lorem Ipsum" },
+    { id: 6, name: "androgynous Skyler", other: "Lorem Ipsum" },
+    { id: 7, name: <a href="#frank">fancy Frank</a>, other: "Lorem Ipsum" },
+  ]);
+
   const [component, react, html] = useMemo(() => {
-    const element = <div>TODO</div>;
+    const element = (
+      <Table
+        columns={columns}
+        data={data}
+        handleRefresh={() => {
+          setData(data.slice(1));
+        }}
+      />
+    );
 
     return [
       element,
       prettyPrintReactElementToString(element),
       prettyPrintReactElementToHtml(element),
     ];
-  }, []);
+  }, [data]);
 
   return (
     <Playground
@@ -37,7 +69,9 @@ export const PlaygroundImplementation = () => {
         storybook,
       }}
     >
-      {component}
+      <div className={styles["table-playground-implementation-wrapper"]}>
+        {component}
+      </div>
     </Playground>
   );
 };
