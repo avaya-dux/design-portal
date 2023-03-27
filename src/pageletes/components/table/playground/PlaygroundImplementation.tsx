836a18c197a769099ac07b59d2e422223d42a868
Joe S <jsebast@avaya.com>
2023-03-27 11:05:28 -0500

    complete work on playground

--- src/pageletes/components/table/playground/PlaygroundImplementation.tsx
+++ src/pageletes/components/table/playground/PlaygroundImplementation.tsx
@@ -1,11 +1,8 @@
 import { useMemo, useState } from "react";
 
 import { Playground } from "components/react";
-import {
-  prettyPrintReactElementToHtml,
-  prettyPrintReactElementToString,
-} from "helpers";
-import { Table } from "@avaya/neo-react";
+import { prettyPrintReact, prettyPrintReactElementToHtml } from "helpers";
+import { Checkbox, CheckboxGroup, Table } from "@avaya/neo-react";
 
 import styles from "./PlaygroundImplementation.module.css";
 
@@ -32,33 +29,68 @@ export const PlaygroundImplementation = () => {
     { id: 4, name: "madam Hailey", other: "Lorem Ipsum" },
     { id: 5, name: "intersex Alex", other: "Lorem Ipsum" },
     { id: 6, name: "androgynous Skyler", other: "Lorem Ipsum" },
-    { id: 7, name: <a href="#frank">fancy Frank</a>, other: "Lorem Ipsum" },
+    { id: 7, name: "fancy Frank", other: "Lorem Ipsum" },
   ]);
+  const [compact, setCompact] = useState(false);
+  const [selectable, setSelectable] = useState(false);
+  const [showPagination, setShowPagination] = useState(false);
 
   const [component, react, html] = useMemo(() => {
     const element = (
       <Table
         columns={columns}
         data={data}
-        handleRefresh={() => {
-          setData(data.slice(1));
-        }}
+        handleRefresh={() => setData(data.slice(1))}
+        rowHeight={compact ? "compact" : "large"}
+        selectableRows={selectable ? "single" : "none"}
+        showPagination={showPagination}
       />
     );
 
-    return [
-      element,
-      prettyPrintReactElementToString(element),
-      prettyPrintReactElementToHtml(element),
-    ];
-  }, [data]);
+    const reactString = prettyPrintReact(`
+<Table
+  columns={${JSON.stringify(columns, null, 2)}}
+  data={${JSON.stringify(data, null, 2)}}
+  handleRefresh={() => setData(data.slice(1))}
+  rowHeight="${compact ? "compact" : "large"}"
+  selectableRows="${selectable ? "single" : "none"}"
+  showPagination={${showPagination}}
+/>
+    `);
+
+    return [element, reactString, prettyPrintReactElementToHtml(element)];
+  }, [compact, data, selectable, showPagination]);
 
   return (
     <Playground
       options={
         <Playground.OptionsContainer>
-          <Playground.OptionsSection title="TODO">
-            <div>TODO</div>
+          <Playground.OptionsSection
+            id="table-state-variables-title"
+            title="Variables"
+          >
+            <CheckboxGroup
+              groupName="table-state-variables"
+              aria-labelledby="table-state-variables-title"
+            >
+              <Checkbox value="compact" onChange={() => setCompact(!compact)}>
+                Compact
+              </Checkbox>
+
+              <Checkbox
+                value="selectable"
+                onChange={() => setSelectable(!selectable)}
+              >
+                Selectable
+              </Checkbox>
+
+              <Checkbox
+                value="show-pagination"
+                onChange={() => setShowPagination(!showPagination)}
+              >
+                Show Pagination
+              </Checkbox>
+            </CheckboxGroup>
           </Playground.OptionsSection>
         </Playground.OptionsContainer>
       }
