e9ea560c59e328b473760cfdb0a3cdb43f352b4d
Joe S <jsebast@avaya.com>
2023-04-11 10:48:43 -0500

    EXAMPLE: **DO NOT MERGE**

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -8,6 +8,19 @@ export const prettyPrintReactElementToHtml = (element: ReactElement) => {
   return prettyPrintHtml(ReactDOMServer.renderToStaticMarkup(element));
 };
 
+/**
+ * This function converts a React element to a string in JSX format with optional formatting options.
+ * @param {ReactElement} element - The React element that you want to convert to a string
+ * representation. This can be any valid React element, including functional components, class
+ * components, and JSX elements.
+ * @param {Options} [options] - The `options` parameter is an optional object that can be passed to the
+ * `prettyPrintReactElementToString` function to customize the output of the JSX string. It can include
+ * the following properties:
+ * @returns The function `prettyPrintReactElementToString` returns a string representation of a React
+ * element in JSX syntax, using the `react-element-to-jsx-string` library. The function takes two
+ * parameters: `element`, which is the React element to be converted to a string, and `options`, which
+ * is an optional object containing configuration options for the conversion.
+ */
 export const prettyPrintReactElementToString = (
   element: ReactElement,
   options?: Options
