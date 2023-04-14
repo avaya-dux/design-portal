f0f3af59a471b43de5f24377b4a692b567045a5b
JoeS <55896546+joe-s-avaya@users.noreply.github.com>
2023-04-14 16:57:44 -0500

    fix image issue on smaller screens (#229)
    * remove dupe background color
    
    * remove lies about the menu button
    
    * remove dupe image
    
    * undo bad change
    
    ---------
    
    Co-authored-by: Joe S <jsebast@avaya.com>

--- src/components/react/LeftNavigation/LeftNavigation.tsx
+++ src/components/react/LeftNavigation/LeftNavigation.tsx
@@ -164,31 +164,6 @@ const LeftNavigationTopElement = ({
           ref={closeButtonRef}
         ></button>
       </div>
-      <a href="/" aria-label="Homepage">
-        <picture>
-          <source
-            media="(max-width: 799px) and (prefers-color-scheme: dark)"
-            srcSet="/imgs/logo-mobile-dark.svg"
-          />
-          <source
-            media="(max-width: 799px)"
-            srcSet="/imgs/logo-mobile-light.svg"
-          />
-          <source
-            media="(max-width: 1279px) and (prefers-color-scheme: dark)"
-            srcSet="/imgs/logo-condensed-dark.svg"
-          />
-          <source
-            media="(max-width: 1279px)"
-            srcSet="/imgs/logo-condensed-light.svg"
-          />
-          <source
-            media="(min-width: 1280px) and (prefers-color-scheme: dark)"
-            srcSet="/imgs/logo-full-dark.svg"
-          />
-          <img src="/imgs/logo-full-light.svg" alt="Avaya Logo" />
-        </picture>
-      </a>
     </div>
   );
 };
