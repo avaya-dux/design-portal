453b2ab1bc84886d7b51219e8fc738d2d17285aa
mrazian85 <61242168+mrazian85@users.noreply.github.com>
2023-04-28 12:46:25 -0400

    NEO-1458: Updated Toast page (#238)
    * Updated Toast page
    
    * Picked up PR feedback

---
+++ src/pageletes/components/toast/sections/index.ts
@@ -0,0 +1,23 @@
+import Intro from "./Intro.astro";
+
+export { Intro };
+
+import Anatomy from "./Anatomy.astro";
+
+export { Anatomy };
+
+import Behavior from "./Behavior.astro";
+
+export { Behavior };
+
+import Specs from "./Specs.astro";
+
+export { Specs };
+
+import RelatedContent from "./RelatedContent.astro";
+
+export { RelatedContent };
+
+import ToastPlayground from "./ToastPlayground.astro";
+
+export { ToastPlayground };
