e5ddc8a15d456253da59c18e265562f155073728
mrazian85 <61242168+mrazian85@users.noreply.github.com>
2023-07-18 11:16:37 -0400

    NEO-1457 Part #1: Page copy and images (#299)
    * Began building out List page
    
    * Added all copy to List page, created a placeholder page for Tabs
    
    * Fixed error in GroupedContent Don't section
    
    * Fixed issue with PageleteSubSection tags in Behavior section

--- src/pageletes/components/list/sections/index.ts
+++ src/pageletes/components/list/sections/index.ts
@@ -1,2 +1,21 @@
 import Intro from "./Intro.astro";
+
 export { Intro };
+
+import { Anatomy } from "./Anatomy";
+export { Anatomy };
+
+import { Behavior } from "./Behavior";
+export { Behavior };
+
+import Specs from "./Specs.astro";
+
+export { Specs };
+
+import KeyboardInteractions from "./KeyboardInteractions.astro";
+
+export { KeyboardInteractions };
+
+import RelatedContent from "./RelatedContent.astro";
+
+export { RelatedContent };
