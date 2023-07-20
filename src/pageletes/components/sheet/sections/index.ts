1e6b0914294624f04844a2858c081528d073bed6
Enrique Prado <enriquep@avaya.com>
2023-07-20 13:30:32 -0700

    Completed Anatomy section, started Behavior

--- src/pageletes/components/sheet/sections/index.ts
+++ src/pageletes/components/sheet/sections/index.ts
@@ -1,2 +1,8 @@
 import Intro from "./Intro.astro";
 export { Intro };
+
+import Anatomy from "./Anatomy.astro";
+export { Anatomy };
+
+import Behavior from "./Behavior.astro";
+export { Behavior };
