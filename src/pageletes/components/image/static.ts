915de915c3c0c4ba195e7d70dc25c9ee4ebc5bc7
mrazian85 <mrazian@avaya.com>
2023-07-21 11:54:05 -0400

    Made Image page improvements

--- src/pageletes/components/image/static.ts
+++ src/pageletes/components/image/static.ts
@@ -1,11 +1,38 @@
 import type { TopicsDict } from "components";
 
-type ImageTopicNames = "playground";
+type ImageTopicNames =
+  | "playground"
+  | "type"
+  | "use"
+  | "accessibility"
+  | "related";
+
+const component = "image";
 
 export const topics: TopicsDict<ImageTopicNames> = {
   playground: {
     order: 0,
-    id: "image-playground",
+    id: `${component}-playground`,
     title: "Interactive Playground",
+  },
+  type: {
+    order: 0,
+    id: `${component}-type`,
+    title: "Type",
+  },
+  use: {
+    order: 0,
+    id: `${component}-when-to-use`,
+    title: "When to Use",
+  },
+  accessibility: {
+    order: 0,
+    id: `${component}-accessibility`,
+    title: "Accessibility",
+  },
+  related: {
+    order: 0,
+    id: `${component}-related`,
+    title: "Related Content",
   },
 };
