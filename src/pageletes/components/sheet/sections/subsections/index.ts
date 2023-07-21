1b0008c00e37fcc6a446e2ce90364eb15a02910e
Enrique Prado <enriquep@avaya.com>
2023-07-21 14:07:37 -0700

    NEO-1512 Sheet component page Part 1 (#304)
    * Adding Sheet component page
    
    * Completed Anatomy section, started Behavior
    
    * Completed Interaction and Apperance subsections
    
    * Completed Buttons subsection
    
    and id fixes
    
    * Completed Placement subsection
    
    * Completed Overlay subsection
    
    * Behavior section complete
    
    * Completed Specs section
    
    * Completed Keyboard Interactions section
    
    * Completed Related Content section
    
    * yarn all and linter clean up
    
    * Added 2 props to make StackedLayout component more flexible
    
    * Fixed background color issue

---
+++ src/pageletes/components/sheet/sections/subsections/index.ts
@@ -0,0 +1,20 @@
+import Interaction from "./Interaction.astro";
+export { Interaction };
+
+import Appearance from "./Appearance.astro";
+export { Appearance };
+
+import Buttons from "./Buttons.astro";
+export { Buttons };
+
+import Placement from "./Placement.astro";
+export { Placement };
+
+import Overlay from "./Overlay.astro";
+export { Overlay };
+
+import Scrolling from "./Scrolling.astro";
+export { Scrolling };
+
+import Dismissal from "./Dismissal.astro";
+export { Dismissal };
