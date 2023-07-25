749502253a756d5bcbcf142d6cdc159e3f915a05
mrazian85 <mrazian@avaya.com>
2023-07-25 15:44:04 -0400

    Built Modal page, edited link style of InfoTable header cell links as per Figma spec

---
+++ src/pageletes/components/modal/sections/index.ts
@@ -0,0 +1,27 @@
+import Intro from "./Intro.astro";
+
+export { Intro };
+
+import Anatomy from "./Anatomy.astro";
+
+export { Anatomy };
+
+import Type from "./Type.astro";
+
+export { Type };
+
+import Behavior from "./Behavior/Behavior.astro";
+
+export { Behavior };
+
+import Specs from "./Specs.astro";
+
+export { Specs };
+
+import KeyboardInteractions from "./KeyboardInteractions.astro";
+
+export { KeyboardInteractions };
+
+import RelatedContent from "./RelatedContent.astro";
+
+export { RelatedContent };
