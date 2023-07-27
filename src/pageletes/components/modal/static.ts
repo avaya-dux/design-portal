fac8dbea51e527193117e1e25f4d492f7963a54c
mrazian85 <61242168+mrazian85@users.noreply.github.com>
2023-07-27 10:53:03 -0400

    NEO-1552 Part 1: Modal page copy and images (#306)
    * Built Modal page, edited link style of InfoTable header cell links as per Figma spec
    
    * Removed commented code, formatted file

---
+++ src/pageletes/components/modal/static.ts
@@ -0,0 +1,38 @@
+import type { TopicsDict } from "components";
+
+type AvatarTopicNames =
+  | "playground"
+  | "anatomy"
+  | "type"
+  | "behavior"
+  | "specs"
+  | "keyboard"
+  | "related";
+
+const component = "modal";
+
+export const topics: TopicsDict<AvatarTopicNames> = {
+  playground: {
+    order: 0,
+    id: `${component}-playground`,
+    title: "Interactive Playground",
+  },
+  anatomy: { order: 1, id: `${component}-anatomy`, title: "Anatomy" },
+  type: { order: 3, id: `${component}-type`, title: "Type" },
+  behavior: { order: 4, id: `${component}-behavior`, title: "Behavior" },
+  specs: {
+    order: 5,
+    id: `${component}-specs`,
+    title: "Specs",
+  },
+  keyboard: {
+    order: 6,
+    id: `${component}-keyboard`,
+    title: "Keyboard Interactions",
+  },
+  related: {
+    order: 7,
+    id: `${component}-related`,
+    title: "Related Content",
+  },
+};
