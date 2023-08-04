7b662b829239092147df77a4dcef5eccd17cb9ef
Enrique Prado <enriquep@avaya.com>
2023-08-04 15:26:38 -0700

    NEO-537: Side navigation part1 (#312)
    * Added Side Nav main structure
    
    Also Intro and Anatomy pages
    
    * Anatomy and States sections done
    
    * Completed Behavior/Icons subsection
    
    * Completed Condensed subsection
    
    * Completed scrolling subsection
    
    * completed scrolling subsection
    
    * done with Specs, Keyboard and Related content sections
    
    * Matt review fixes
    
    * CR fixes
    
    * More CR fixes
    
    * clean up
    
    * Fixed typo
    
    * fixing padding
    
    * fixed typo

---
+++ src/pageletes/components/side-navigation/sections/subsections/index.ts
@@ -0,0 +1,11 @@
+import Icons from "./Icons.astro";
+export { Icons };
+
+import Condensed from "./Condensed.astro";
+export { Condensed };
+
+import TextWrap from "./TextWrap.astro";
+export { TextWrap };
+
+import Scrolling from "./Scrolling.astro";
+export { Scrolling };
