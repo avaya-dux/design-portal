73844b09d1cd0c3c2d25ada0247d6d0135dc0d3c
Enrique Prado <enriquep@avaya.com>
2023-08-17 17:25:11 -0700

    Keyboard and Related content sections done

--- src/pageletes/components/breadcrumb/sections/index.ts
+++ src/pageletes/components/breadcrumb/sections/index.ts
@@ -12,3 +12,9 @@ export { Behavior };
 
 import Specs from "./Specs.astro";
 export { Specs };
+
+import KeyboardInteractions from "./KeyboardInteractions.astro";
+export { KeyboardInteractions };
+
+import Related from "./Related.astro";
+export { Related };
