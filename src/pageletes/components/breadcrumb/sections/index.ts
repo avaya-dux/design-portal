0e7a6764705ff168de2ca98cd1873f270b296496
Enrique Prado <enriquep@avaya.com>
2023-08-28 12:33:02 -0700

    NEO-1602: Breadcrumb page part 1 (#317)
    * Added breadcrumb page with Intro and Anatomy
    
    * States section done
    
    * Behavior-Placement subsection done
    
    * behavior-overflow subsection done
    
    * behavior-truncate subsection done
    
    * behavior-buttons subsection done
    
    * Specs section done
    
    * Keyboard and Related content sections done
    
    * yarn all formatting
    
    * final clean up
    
    * CR fix
    
    * Fixed Matt's feedback
    
    * Fixed padding
    
    * fixed typo

---
+++ src/pageletes/components/breadcrumb/sections/index.ts
@@ -0,0 +1,20 @@
+import Intro from "./Intro.astro";
+export { Intro };
+
+import Anatomy from "./Anatomy.astro";
+export { Anatomy };
+
+import States from "./States.astro";
+export { States };
+
+import Behavior from "./Behavior.astro";
+export { Behavior };
+
+import Specs from "./Specs.astro";
+export { Specs };
+
+import KeyboardInteractions from "./KeyboardInteractions.astro";
+export { KeyboardInteractions };
+
+import Related from "./Related.astro";
+export { Related };
