5b3400d46cf57ab4bca82aac4f21a96637265c9a
Joe Sebast <joe.sebast.avaya@gmail.com>
2023-08-31 12:02:30 -0500

    merge icon pages playground implementation

--- src/pageletes/components/icon/playground/PlaygroundImplementation.tsx
+++ src/pageletes/components/icon/playground/PlaygroundImplementation.tsx
@@ -1,37 +1,61 @@
-import { Icon, type IconProps, Radio, RadioGroup } from "@avaya/neo-react";
+import type { IconProps } from "@avaya/neo-react";
+import {
+  Icon,
+  Radio,
+  RadioGroup,
+  Select,
+  SelectOption,
+} from "@avaya/neo-react";
 import clsx from "clsx";
 import { useMemo, useState } from "react";
 
 import { Playground } from "components/react";
-import { prettyPrintHtml, prettyPrintReact } from "helpers";
+import {
+  prettyPrintHtml,
+  prettyPrintReact,
+  prettyPrintReactElementToString,
+} from "helpers";
 
-export const sandbox = "https://codesandbox.io/s/neo-react-icon-tywncu";
+export const sandbox = "https://codesandbox.io/s/neo-react-icon-jvhyst";
 export const storybook =
-  "https://neo-react-library-storybook.netlify.app/?path=/story/components-icon--standard-icon-sm";
+  "https://neo-react-library-storybook.netlify.app/?path=/story/components-icon";
 
 export const PlaygroundImplementation = () => {
-  const [size, setSize] = useState<IconProps["size"]>("md");
+  const [size, setSize] = useState<IconProps["size"]>("lg");
+  const [status, setStatus] = useState<IconProps["status"]>("none");
 
-  const [react, html] = useMemo(() => {
-    const react = prettyPrintReact(
-      `
-    <Icon aria-label="info icon" icon="info" size="${size}"/>
-`,
+  const [element, react, html] = useMemo(() => {
+    const element = (
+      <Icon
+        aria-label="info icon"
+        icon="info"
+        size={size}
+        status={status === "none" ? null : status}
+      />
     );
+
+    const react = prettyPrintReactElementToString(element);
+
+    const sizeCssWord =
+      size === "lg" ? "large" : size === "md" ? "medium" : "small";
+    const statusCssWord = status === "none" ? "" : "-state";
+    const iconSize = `neo-icon${statusCssWord}--${sizeCssWord}`;
 
     const html = prettyPrintHtml(
       `
-      <span class="${clsx(
-        "neo-icon-info",
-        size === "sm" && "neo-icon--small",
-        size === "md" && "neo-icon--medium",
-        size === "lg" && "neo-icon--large",
-      )}"></span>
+      <span
+        role="img"
+        aria-label="info icon"
+        class="${clsx(
+          "neo-icon-info",
+          iconSize,
+          status !== "none" && `neo-icon-state--${status}`,
+        )}"></span>
       `,
     );
 
-    return [react, html];
-  }, [size]);
+    return [element, react, html];
+  }, [size, status]);
 
   return (
     <Playground
@@ -50,6 +74,20 @@ export const PlaygroundImplementation = () => {
               <Radio value="lg">Large</Radio>
             </RadioGroup>
           </Playground.OptionsSection>
+
+          <Playground.OptionsSection title="Status">
+            <Select
+              aria-label="Status"
+              value={status}
+              onChange={(value) => setStatus(value as IconProps["status"])}
+            >
+              {["none", "available", "away", "busy"].map((value) => (
+                <SelectOption key={value} value={value}>
+                  {value}
+                </SelectOption>
+              ))}
+            </Select>
+          </Playground.OptionsSection>
         </Playground.OptionsContainer>
       }
       examples={{
@@ -59,7 +97,7 @@ export const PlaygroundImplementation = () => {
         storybook,
       }}
     >
-      <Icon aria-label="info-icon" icon="info" size={size}></Icon>
+      {element}
     </Playground>
   );
 };
