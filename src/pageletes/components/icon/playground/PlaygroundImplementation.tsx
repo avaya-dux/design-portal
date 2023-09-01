05a8dbe4c69087ee1623413ea4c12a87dfe04b00
Joe Sebast <joe.sebast.avaya@gmail.com>
2023-09-01 17:14:31 -0500

    updates per Matt's request

--- src/pageletes/components/icon/playground/PlaygroundImplementation.tsx
+++ src/pageletes/components/icon/playground/PlaygroundImplementation.tsx
@@ -5,10 +5,9 @@ import {
   RadioGroup,
   Select,
   SelectOption,
-  removePopupManagerContainer,
-  usePopup,
 } from "@avaya/neo-react";
-import { useEffect, useMemo, useState } from "react";
+import clsx from "clsx";
+import { useMemo, useState } from "react";
 
 import { Playground } from "components/react";
 import {
@@ -20,16 +19,12 @@ export const sandbox = "https://codesandbox.io/s/neo-react-icon-jvhyst";
 export const storybook =
   "https://neo-react-library-storybook.netlify.app/?path=/story/components-icon";
 
-export const PlaygroundImplementation = () => {
-  useEffect(() => {
-    return () => {
-      removePopupManagerContainer();
-    };
-  }, []);
-  const { toast } = usePopup("interactive-toast");
+type StatusType = "Icon" | "Icon with Status";
 
+export const PlaygroundImplementation = () => {
+  const [type, setType] = useState<StatusType>("Icon");
   const [size, setSize] = useState<IconProps["size"]>("lg");
-  const [status, setStatus] = useState<IconProps["status"]>("none");
+  const [status, setStatus] = useState<IconProps["status"]>("available");
 
   const [element, react, html] = useMemo(() => {
     const element = (
@@ -52,45 +47,71 @@ export const PlaygroundImplementation = () => {
     <Playground
       options={
         <Playground.OptionsContainer>
-          <Playground.OptionsSection title="Size">
-            <RadioGroup
-              groupName="size"
-              selected={size}
-              onChange={(e) => {
-                setSize(e.target.value as IconProps["size"]);
-              }}
-            >
-              <Radio value="sm" disabled={status !== "none"}>
-                Small
-              </Radio>
-              <Radio value="md">Medium</Radio>
-              <Radio value="lg">Large</Radio>
-            </RadioGroup>
-          </Playground.OptionsSection>
-
-          <Playground.OptionsSection title="Status">
+          <Playground.OptionsSection title="Type">
             <Select
-              aria-label="Status"
-              value={status}
-              onChange={(value) => {
-                setStatus(value as IconProps["status"]);
-                if (value !== "none" && size === "sm") {
+              aria-label="Type"
+              value={type}
+              onChange={(value: StatusType) => {
+                if (value === "Icon with Status" && size === "sm") {
                   setSize("md");
-                  toast({
-                    position: "top-right",
-                    message:
-                      "NOTE: Icons with a status must be size medium or large.",
-                  });
                 }
+
+                setType(value);
               }}
             >
-              {["none", "available", "away", "busy"].map((value) => (
-                <SelectOption key={value} value={value}>
-                  {value}
-                </SelectOption>
-              ))}
+              <SelectOption key="Icon" value="Icon">
+                Icon
+              </SelectOption>
+
+              <SelectOption key="Icon with Status" value="Icon with Status">
+                Icon with Status
+              </SelectOption>
             </Select>
           </Playground.OptionsSection>
+
+          {/*
+            HACK: if I hide the "Small" radio button, styling breaks or the logic
+            inside of the react component breaks. Having to do this hack to get it
+            working as Matt wants it.
+          */}
+          <Playground.OptionsSection title="Size">
+            <div className={clsx(type === "Icon with Status" && "hidden")}>
+              <RadioGroup
+                groupName="size-for-icon-with-status"
+                selected={size}
+                onChange={(e) => setSize(e.target.value as IconProps["size"])}
+              >
+                <Radio value="sm">Small</Radio>
+                <Radio value="md">Medium</Radio>
+                <Radio value="lg">Large</Radio>
+              </RadioGroup>
+            </div>
+
+            <div className={clsx(type === "Icon" && "hidden")}>
+              <RadioGroup
+                groupName="size-for-icon-without-status"
+                selected={size}
+                onChange={(e) => setSize(e.target.value as IconProps["size"])}
+              >
+                <Radio value="md">Medium</Radio>
+                <Radio value="lg">Large</Radio>
+              </RadioGroup>
+            </div>
+          </Playground.OptionsSection>
+
+          {type === "Icon with Status" && (
+            <Playground.OptionsSection title="Status">
+              <RadioGroup
+                groupName="status"
+                selected={status}
+                onChange={(e) => setStatus(e.target.value)}
+              >
+                <Radio value="available">Available</Radio>
+                <Radio value="away">Away</Radio>
+                <Radio value="busy">Busy</Radio>
+              </RadioGroup>
+            </Playground.OptionsSection>
+          )}
         </Playground.OptionsContainer>
       }
       examples={{
