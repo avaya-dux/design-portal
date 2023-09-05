711ce668588b8a193540fae96de74a52edbda02b
Brayden Wachowiak <bjwachowiak@avaya.com>
2023-09-05 14:53:35 -0500

    Removed unused imports and removed dead code.

--- src/pageletes/components/pagination/playground/PlaygroundImplementation.tsx
+++ src/pageletes/components/pagination/playground/PlaygroundImplementation.tsx
@@ -1,10 +1,6 @@
-import {
-  Radio,
-  RadioGroup,
-  Pagination,
-} from "@avaya/neo-react";
+import { Radio, RadioGroup, Pagination } from "@avaya/neo-react";
 
-import {useState, useLayoutEffect, useRef} from "react";
+import { useState } from "react";
 
 import { Playground } from "components/react";
 
@@ -14,57 +10,36 @@ import {
 } from "helpers";
 
 const storybook =
-"https://neo-react-library-storybook.netlify.app/?path=/story/components-checkbox";
+  "https://neo-react-library-storybook.netlify.app/?path=/docs/components-pagination--docs";
 
+// Update URL
 const sandbox = "https://codesandbox.io/s/neo-react-checkbox-kb5gbt";
 
 type PaginationTypeOption = "full" | "condensed";
 
 export const PlaygroundImplementation = () => {
-
-  const [paginationOption, setPaginationOption] = useState<PaginationTypeOption>("full");
+  const [paginationOption, setPaginationOption] =
+    useState<PaginationTypeOption>("full");
   const [setIndex, setPageIndex] = useState(1);
-  console.log("setIndex: ", setIndex);
   const [itemsPerPage, setItemsPerPage] = useState(5);
   const itemCount = 20;
 
-  const [rootWidth, setRootWidth] = useState(0);
-  const rootRef = useRef<HTMLDivElement>(null);
-
-  useLayoutEffect(() => {
-    const updateRootWidth = () => {
-      if (rootRef.current) {
-        setRootWidth(rootRef.current.offsetWidth);
-      }
-    };
-
-    window.addEventListener("resize", updateRootWidth);
-    updateRootWidth();
-    return () => window.removeEventListener("resize", updateRootWidth);
-  }, [rootRef]);
-
   const element = (
-    <Pagination 
+    <Pagination
       currentPageIndex={setIndex}
       itemCount={itemCount}
       itemsPerPage={itemsPerPage}
       itemsPerPageOptions={[1, 5, 10]}
-      itemDisplayType={'page'}
-      paginationRootWidth={rootWidth}
-
+      itemDisplayType={"page"}
       onPageChange={(e, newIndex) => {
-        console.log("OnPage: ", newIndex);
         e?.preventDefault();
         setPageIndex(newIndex);
       }}
-
       onItemsPerPageChange={(e, newItemsPerPage) => {
         e?.preventDefault();
         setItemsPerPage(newItemsPerPage);
 
-        const maxPageIndex = Math.ceil(
-          itemCount / newItemsPerPage
-        );
+        const maxPageIndex = Math.ceil(itemCount / newItemsPerPage);
         if (setIndex > maxPageIndex) {
           setPageIndex(maxPageIndex);
         }
