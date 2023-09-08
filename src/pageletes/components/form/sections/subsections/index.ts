303acf973acafb999564e2c64a2549519e90810d
Enrique Prado <enriquep@avaya.com>
2023-09-08 16:32:40 -0700

    Added 3 subsections

--- src/pageletes/components/form/sections/subsections/index.ts
+++ src/pageletes/components/form/sections/subsections/index.ts
@@ -1,2 +1,8 @@
 import Alignment from "./Alignment.astro";
 export { Alignment };
+
+import Spacing from "./Spacing.astro";
+export { Spacing };
+
+import ButtonPlacement from "./ButtonPlacement.astro";
+export { ButtonPlacement };
