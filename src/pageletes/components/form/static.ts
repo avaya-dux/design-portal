ab443b1e6464b01f796de6bcbef9d354c881b35d
Enrique Prado <enriquep@avaya.com>
2023-09-11 13:25:15 -0700

    yarn all formatting

--- src/pageletes/components/form/static.ts
+++ src/pageletes/components/form/static.ts
@@ -4,11 +4,7 @@ export const sandbox = "https://codesandbox.io/";
 export const storybook =
   "https://neo-react-library-storybook.netlify.app/?path=/story/";
 
-type BreadcrumbTopicNames =
-  | "playground"
-  | "anatomy"
-  | "behavior"
-  | "related";
+type BreadcrumbTopicNames = "playground" | "anatomy" | "behavior" | "related";
 
 export const topics: TopicsDict<BreadcrumbTopicNames> = {
   playground: {
