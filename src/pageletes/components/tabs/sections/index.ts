15ea99fc2146890603d68bd63b191f6bc449ea76
Chunguang Yang <chungguangy@avaya.com>
2023-09-11 09:54:39 -0400

    add intro and index

--- src/pageletes/components/tabs/sections/index.ts
+++ src/pageletes/components/tabs/sections/index.ts
@@ -1,3 +1,33 @@
 import Intro from "./Intro.astro";
 
 export { Intro };
+
+import Playground from "./Playground.astro";
+
+export { Playground };
+
+import Anatomy from "./Anatomy.astro";
+
+export { Anatomy };
+
+import Type from "./Type.astro";
+
+export { Type };
+
+import Behavior from "./Behavior.astro";
+
+export { Behavior };
+
+import Specs from "./Specs.astro";
+
+export { Specs };
+
+import KeyboardInteractions from "./KeyboardInteractions.astro";
+
+export { KeyboardInteractions };
+
+import Related from "./Related.astro";
+
+export { Related };
+
+
