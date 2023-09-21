e6c429d821e2052b8021a2ba3cf5d7ce985b5593
Brayden Wachowiak <142830426+Brayden-Wachowiak@users.noreply.github.com>
2023-09-21 12:26:28 -0500

    principles page implementation PR (#345)
    * principles page implementation PR
    
    * Links now open to new tabs
    
    * Changed Avaya related links to not open a new tab
    
    * run `yarn all` (#347)
    
    * Changed some variable names and added proper spacing
    
    * commit latest
    
    * add back eslintrc file
    
    * Changed the name of a style tag
    
    * Changed a style tag
    
    * Changed TypeList class name
    
    * Fixed some typos
    
    ---------
    
    Co-authored-by: JoeS <55896546+joe-s-avaya@users.noreply.github.com>

---
+++ src/pageletes/accessibility/principles/sections/index.ts
@@ -0,0 +1,20 @@
+import Intro from "./Intro.astro";
+export { Intro };
+
+import Accessibility from "./Accessibility.astro";
+export { Accessibility };
+
+import Regulations from "./Regulations.astro";
+export { Regulations };
+
+import TypesLayout from "./types-frame-layout/TypesLayout.astro";
+export { TypesLayout };
+
+import TwoColumnLayout from "./types-frame-layout/TwoColumnLayout.astro";
+export { TwoColumnLayout };
+
+import Types from "./Types.astro";
+export { Types };
+
+import TypeList from "./types-frame-layout/TypeList.astro";
+export { TypeList };
