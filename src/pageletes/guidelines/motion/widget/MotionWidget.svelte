a9d8958405c023c7537cf2d627e69d65aba9c4dd
Enrique Prado <enriquep@avaya.com>
2023-09-28 16:44:45 -0700

    Border and basic layout

--- src/pageletes/guidelines/motion/widget/MotionWidget.svelte
+++ src/pageletes/guidelines/motion/widget/MotionWidget.svelte
@@ -3,11 +3,24 @@
 	let visible = true;
 </script>
 
-<label>
-	<input type="checkbox" bind:checked={visible} />
-	visible
-</label>
+<div class="widget-container">
+  <div class="options-panel">
+    <label>
+      <input type="checkbox" bind:checked={visible} />
+      visible
+    </label>
+  </div>
+  <div class="animation-panel">
+    {#if visible}
+      <p in:fly={{ y: 100, duration: 2000 }} out:fade>Flies in, fades out</p>
+    {/if}
+  </div>
+</div>
 
-{#if visible}
-	<p in:fly={{ y: 200, duration: 2000 }} out:fade>Flies in, fades out</p>
-{/if}
+
+<style>
+.widget-container {
+  border: 1px solid var(--neo-color-base-200);
+  height: 400px;
+}
+</style>
