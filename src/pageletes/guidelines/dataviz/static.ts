9b4be3553700fef9a9b1b93ee7c951bc5a565f97
Enrique Prado <enriquep@avaya.com>
2023-10-10 12:24:33 -0700

    Added Data Visualization page
    and Intro section

---
+++ src/pageletes/guidelines/dataviz/static.ts
@@ -0,0 +1,22 @@
+import type { TopicsDict } from "components";
+
+type DataVizTopicNames = "guidelines" | "linecharts" | "barcharts" | "piecharts";
+
+export const topics: TopicsDict<DataVizTopicNames> = {
+  guidelines: {
+    order: 0,
+    id: "dataviz-guidelines",
+    title: "Common Guidelines",
+  },
+  linecharts: { order: 1, id: "dataviz-line-charts", title: "Line Charts" },
+  barcharts: {
+    order: 2,
+    id: "dataviz-bar-charts",
+    title: "Bar Charts",
+  },
+  piecharts: {
+    order: 3,
+    id: "dataviz-pie-charts",
+    title: "Pie Charts",
+  },
+};
