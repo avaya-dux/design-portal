65f85a5e20022190cc37d0e0014bea41bb4977f7
Brayden Wachowiak <142830426+Brayden-Wachowiak@users.noreply.github.com>
2023-10-11 10:41:37 -0500

    Platform Page (#371)
    * Changed the InfoList and SubHeader Components for more flexibility and added Platform Page
    
    * Fixed spacing and other visual problems
    
    * Removed dead code

---
+++ src/pageletes/guidelines/platform/sections/index.ts
@@ -0,0 +1,29 @@
+import Intro from "./Intro.astro";
+export { Intro };
+
+import Android from "./Android.astro";
+export { Android };
+
+import Mac from "./Mac.astro";
+export { Mac };
+
+import Web from "./Web.astro";
+export { Web };
+
+import iOS from "./IOS.astro";
+export { iOS };
+
+import Windows from "./Windows.astro";
+export { Windows };
+
+import SectionLayout from "./sections-frame-layout/SectionLayout.astro";
+export { SectionLayout };
+
+import TwoColumnLayout from "./sections-frame-layout/TwoColumnLayout.astro";
+export { TwoColumnLayout };
+
+import Sections from "./Sections.astro";
+export { Sections };
+
+import SectionList from "./sections-frame-layout/SectionList.astro";
+export { SectionList };
