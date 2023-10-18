b6589415c6b251892de4a3445efab293222bc986
Brayden Wachowiak <bjwachowiak@avaya.com>
2023-10-18 10:05:58 -0500

    Pushing current progress. To move to a different branch

---
+++ src/pageletes/components/tree-view/playground/PlaygroundImplementation.tsx
@@ -0,0 +1,40 @@
+import "./pagination-playground.css";
+import { useState } from "react";
+
+import { Playground } from "components/react";
+
+import {
+  prettyPrintReactElementToHtml,
+  prettyPrintReactElementToString,
+} from "helpers";
+
+const storybook =
+  "https://neo-react-library-storybook.netlify.app/?path=/docs/components-pagination--docs";
+
+const sandbox =
+  "https://codesandbox.io/s/neo-react-pagination-r7ffxy?file=/src/App.js";
+
+export const PlaygroundImplementation = () => {
+
+  const element = (
+    <></>
+  );
+
+  return (
+    <Playground
+      options={
+        <Playground.OptionsContainer>
+          <Playground.OptionsSection title="Tree View Options"></Playground.OptionsSection>
+        </Playground.OptionsContainer>
+      }
+      examples={{
+        html: prettyPrintReactElementToHtml(element),
+        react: prettyPrintReactElementToString(element),
+        sandbox,
+        storybook,
+      }}
+    >
+      {element}
+    </Playground>
+  );
+};
