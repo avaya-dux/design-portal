ca5164964faded056ec69b206ef2a8a5fb04db88
yangAtSpoken <31546063+yangAtSpoken@users.noreply.github.com>
2023-10-18 11:06:29 -0400

    NEO-1706 resources and guides page under accessibility (#379)
    * accessibility/resources-guides
    
    * use inline-block to remove linebreak inside link
    
    * fix page title; updated external links
    
    * use ExternalLink

---
+++ src/pageletes/accessibility/resources-guides/sections/index.ts
@@ -0,0 +1,11 @@
+import Intro from "./Intro.astro";
+export { Intro };
+
+import VPAT from "./VPAT.astro";
+export { VPAT };
+
+import GettingStarted from "./GettingStarted.astro";
+export { GettingStarted };
+
+import AdditionalResources from "./AdditonalResources.astro";
+export { AdditionalResources };
