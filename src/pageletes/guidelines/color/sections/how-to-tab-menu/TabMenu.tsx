9d906f88315d358fa6e6f9f04e56752c69da0cc5
Brayden Wachowiak <bjwachowiak@avaya.com>
2023-10-18 21:32:52 -0500

    Added final revisions for the first stage of the PR

--- src/pageletes/guidelines/color/sections/how-to-tab-menu/TabMenu.tsx
+++ src/pageletes/guidelines/color/sections/how-to-tab-menu/TabMenu.tsx
@@ -1,30 +1,18 @@
-
 import { Tabs, TabList, Tab, TabPanels, TabPanel } from "@avaya/neo-react";
 
-export const TabMenu = () => {
-
-    return (
-        <div>
-            <slot name="tab1" slot="tab1"/>
-            <Tabs>
-                <TabList>
-                    <Tab id="tab1">Base</Tab>
-                    <Tab id="tab2">Red</Tab>
-                    <Tab id="tab3">Blue</Tab>
-                    <Tab id="tab4">Green</Tab>
-                    <Tab id="tab5">Orange</Tab>
-                    <Tab id="tab6">Purple</Tab>
-                </TabList>
-                <TabPanels> 
-                    <slot name="tab1" slot="tab1"/>
-                    <TabPanel><slot name="tab1"/></TabPanel>
-                    <TabPanel><slot name="tab2"/></TabPanel>
-                    <TabPanel><slot name="tab3"/></TabPanel>
-                    <TabPanel><slot name="tab4"/></TabPanel>
-                    <TabPanel><slot name="tab5"/></TabPanel>
-                    <TabPanel><slot name="tab6"/></TabPanel>
-                </TabPanels>
-            </Tabs>
-        </div>
-    );
-}
+export const TabMenu = (props) => {
+  return (
+    <Tabs>
+      <TabList>
+        {props.panels.map((panel: string) => (
+          <Tab id={panel}>{panel.charAt(0).toUpperCase() + panel.slice(1)}</Tab>
+        ))}
+      </TabList>
+      <TabPanels>
+        {props.panels.map((panel: string) => (
+          <TabPanel>{props[panel]}</TabPanel>
+        ))}
+      </TabPanels>
+    </Tabs>
+  );
+};
