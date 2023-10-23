c1fe99a6bae5cb946847a03e40fc922e3d767f5b
Brayden Wachowiak <bjwachowiak@avaya.com>
2023-10-23 11:35:17 -0500

    Added Changelog page and components

--- src/pageletes/docs/changelog/index.ts
+++ src/pageletes/docs/changelog/index.ts
@@ -1,2 +1,5 @@
 import Intro from "./Intro.astro";
 export { Intro };
+
+import Info from "./Info.astro";
+export { Info };
---
+++ src/pageletes/docs/changelog/tab-sections/releases/design-updates.tsx
@@ -0,0 +1,37 @@
+export const updates = [
+    {
+        date: "March 10, 2022",
+        sections: [
+            {
+                header: "Design Tokens",
+                points: [
+                    "Two new colors, base-150 and base-850",
+                    "New neo-leftnav__category class and selector for NavCategory component for Lefnav component"
+                ]
+            },
+        ]
+    },
+
+    {
+        date: "January 25, 2022",
+
+        sections: [
+            {
+                header: "Components",
+                points: [
+                    "New classes and styles for additional Pagination component variants",
+                ],
+            },
+
+            {
+                header: "Icons",
+                points: [
+                    "incognito",
+                    "incognito-off",
+                    "layout-immersive"
+                ]
+            }
+
+        ]
+    }
+];
