ea333153e19b2571cd235463631009247d50ec2c
Chunguang Yang <chungguangy@avaya.com>
2023-10-26 19:22:58 -0400

    add color and typography sections

---
+++ src/pageletes/components/design-tokens/components/index.ts
@@ -0,0 +1,11 @@
+import ThreeColumnGrid from "./ThreeColumnGrid.astro";
+export { ThreeColumnGrid };
+
+import ColorColumn from "./ColorColumn.astro";
+export { ColorColumn };
+
+import SingleColor from "./SingleColor.astro";
+export { SingleColor };
+
+import TypographyTable from "./TypographyTable.astro";
+export { TypographyTable };
