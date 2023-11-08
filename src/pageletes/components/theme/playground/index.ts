a4a336d2b78e51588b484d536297684a1dffd360
mrazian85 <61242168+mrazian85@users.noreply.github.com>
2023-11-08 09:23:02 +0400

    NEO-1511: Build theme page (#394)
    * Built theme page
    
    * Re-factored exmaple code to remove unnecessary class names
    
    * Picked up PR feedback, ran yarn all
    
    * Picked up more PR feedback

---
+++ src/pageletes/components/theme/playground/index.ts
@@ -0,0 +1,3 @@
+import { PlaygroundImplementation } from "./ThemePlayground";
+
+export { PlaygroundImplementation };
--- src/pageletes/components/theme/sections/index.ts
+++ src/pageletes/components/theme/sections/index.ts
@@ -1,3 +1,19 @@
 import Intro from "./Intro.astro";
 
 export { Intro };
+
+import Type from "./Type.astro";
+
+export { Type };
+
+import Behavior from "./Behavior.astro";
+
+export { Behavior };
+
+import Related from "./Related.astro";
+
+export { Related };
+
+import PlaygroundImplementation from "./PlaygroundImplementation.astro";
+
+export { PlaygroundImplementation };
