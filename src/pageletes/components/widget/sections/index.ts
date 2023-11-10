485d02e7438b54be858ef84420a340e727df80b8
mrazian85 <mrazian@avaya.com>
2023-11-10 14:13:14 +0400

    Built Widget page except for Playground

---
+++ src/pageletes/components/widget/sections/index.ts
@@ -0,0 +1,23 @@
+import Intro from "./Intro.astro";
+
+export { Intro };
+
+import Anatomy from "./Anatomy.astro";
+
+export { Anatomy };
+
+import Behavior from "./Behavior.astro";
+
+export { Behavior };
+
+import Specs from "./Specs.astro";
+
+export { Specs };
+
+import KeyboardInteractions from "./KeyboardInteractions.astro";
+
+export { KeyboardInteractions };
+
+import Related from "./Related.astro";
+
+export { Related };
---
+++ src/pageletes/components/widget/static.ts
@@ -0,0 +1,44 @@
+import type { TopicsDict } from "components";
+
+type WidgetTopicNames =
+  | "playground"
+  | "anatomy"
+  | "behavior"
+  | "specs"
+  | "keyboard"
+  | "related";
+
+const component = "widget";
+
+export const topics: TopicsDict<WidgetTopicNames> = {
+  playground: {
+    order: 0,
+    id: `${component}-playground`,
+    title: "Interactive Playground",
+  },
+  anatomy: {
+    order: 1,
+    id: `${component}-anatomy`,
+    title: "Anatomy",
+  },
+  behavior: {
+    order: 2,
+    id: `${component}-behavior`,
+    title: "Behavior",
+  },
+  specs: {
+    order: 3,
+    id: `${component}-specs`,
+    title: "Specs",
+  },
+  keyboard: {
+    order: 4,
+    id: `${component}-keyboard`,
+    title: "Keyboard Interactions",
+  },
+  related: {
+    order: 5,
+    id: `${component}-related`,
+    title: "Related Content",
+  },
+};
