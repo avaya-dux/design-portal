2dcf434a55960fa1ef6bfae15afccf1ea5d4b574
Joe Sebast <joe.sebast.avaya@gmail.com>
2023-11-27 14:51:47 -0600

    clean up Accessibility / Principals page, Disabilities section

--- src/pageletes/accessibility/principles/sections/index.ts
+++ src/pageletes/accessibility/principles/sections/index.ts
@@ -7,14 +7,5 @@ export { Accessibility };
 import Regulations from "./Regulations.astro";
 export { Regulations };
 
-import TypesLayout from "./types-frame-layout/TypesLayout.astro";
-export { TypesLayout };
-
-import TwoColumnLayout from "./types-frame-layout/TwoColumnLayout.astro";
-export { TwoColumnLayout };
-
 import Types from "./Types.astro";
 export { Types };
-
-import TypeList from "./types-frame-layout/TypeList.astro";
-export { TypeList };
