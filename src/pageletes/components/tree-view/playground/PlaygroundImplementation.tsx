11af802783c8d5d1e58073a9f270a4abd2d79df4
Joe Sebast <joe.sebast.avaya@gmail.com>
2023-11-29 12:36:52 -0600

    add Playground for the Tree component page

--- src/pageletes/components/tree-view/playground/PlaygroundImplementation.tsx
+++ src/pageletes/components/tree-view/playground/PlaygroundImplementation.tsx
@@ -1,29 +1,78 @@
-import { Playground } from "components/react";
+import { Branch, Leaf, Radio, RadioGroup, Tree } from "@avaya/neo-react";
+import { useMemo, useState } from "react";
 
+import { Playground } from "components/react";
 import {
   prettyPrintReactElementToHtml,
   prettyPrintReactElementToString,
 } from "helpers";
 
 const storybook =
-  "https://neo-react-library-storybook.netlify.app/?path=/docs/components-pagination--docs";
-
+  "https://neo-react-library-storybook.netlify.app/?path=/story/components-tree";
 const sandbox =
-  "https://codesandbox.io/s/neo-react-pagination-r7ffxy?file=/src/App.js";
+  "https://codesandbox.io/s/neo-react-tree-gxwrvs?file=/src/App.js";
 
 export const PlaygroundImplementation = () => {
-  const element = <></>;
+  const [nested, setNested] = useState(true);
+
+  const [element, react, html] = useMemo(() => {
+    const implementation = nested ? (
+      <Tree label="Nested Tree">
+        <Branch title="Branch One (string)">
+          <Leaf>leaf one</Leaf>
+        </Branch>
+
+        <Branch
+          title={
+            <div>
+              <b>Branch Two</b> (div)
+            </div>
+          }
+        >
+          <Leaf>leaf two</Leaf>
+
+          <Branch title="Branch Three (string)">
+            <Leaf>leaf three</Leaf>
+          </Branch>
+
+          <Leaf>leaf four</Leaf>
+        </Branch>
+      </Tree>
+    ) : (
+      <Tree label="Flat Tree">
+        <Leaf>leaf one</Leaf>
+
+        <Leaf>leaf two</Leaf>
+      </Tree>
+    );
+
+    return [
+      implementation,
+      prettyPrintReactElementToString(implementation),
+      prettyPrintReactElementToHtml(implementation),
+    ];
+  }, [nested]);
 
   return (
     <Playground
       options={
         <Playground.OptionsContainer>
-          <Playground.OptionsSection title="Tree View Options"></Playground.OptionsSection>
+          <Playground.OptionsSection title="Tree View Option">
+            <RadioGroup
+              aria-label="Tree View Option"
+              groupName="tree-is-nested"
+              selected={nested ? "true" : "false"}
+              onChange={(e) => setNested(e.target.value === "true")}
+            >
+              <Radio value="true">Nested</Radio>
+              <Radio value="false">Flat</Radio>
+            </RadioGroup>
+          </Playground.OptionsSection>
         </Playground.OptionsContainer>
       }
       examples={{
-        html: prettyPrintReactElementToHtml(element),
-        react: prettyPrintReactElementToString(element),
+        html,
+        react,
         sandbox,
         storybook,
       }}
