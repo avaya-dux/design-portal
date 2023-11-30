14e8ecdd2da5cf3dcea3c56c5b2eda7144d7ffda
JoeS <55896546+joe-s-avaya@users.noreply.github.com>
2023-11-30 09:16:29 -0600

    cleanup code for Testing Tools page (#423)
    * cleanup code for Testing Tools page
    
    * switch to using `ExternalLink` for `Card.astro` component

--- src/pageletes/accessibility/testing-tools/components/index.ts
+++ src/pageletes/accessibility/testing-tools/components/index.ts
@@ -1,8 +1,2 @@
 import Card from "./Card.astro";
 export { Card };
-
-import TwoColumnGrid from "./TwoColumnGrid.astro";
-export { TwoColumnGrid };
-
-import ViewWebsite from "./ViewWebsite.astro";
-export { ViewWebsite };
