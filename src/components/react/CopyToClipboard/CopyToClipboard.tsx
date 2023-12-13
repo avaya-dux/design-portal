660027d053363daa656e4a1f91b64add8154fd9f
Enrique Prado <enriquep@avaya.com>
2023-12-13 13:25:45 -0800

    NEO-1948: Copy Link button improvements (#448)
    * basic functionality done
    
    notification not working
    
    * Adding duration and toast message props
    
    Now using Toast instead of Notification
    
    * Final clean up
    
    * Fixing toast issue
    
    * Added Tooltip per Matt's request
    
    * formatting
    
    * yarn all

---
+++ src/components/react/CopyToClipboard/CopyToClipboard.tsx
@@ -0,0 +1,55 @@
+import {
+  IconButton,
+  removePopupManagerContainer,
+  Tooltip,
+  usePopup,
+} from "@avaya/neo-react";
+import { copyTextToClipboard } from "../utils";
+import { useEffect } from "react";
+
+export interface CopyToClipbardProps {
+  text: string;
+  duration?: number;
+  message?: string;
+}
+
+export const CopyToClipboard = ({
+  text,
+  duration = 2000,
+  message = "Link copied to clipboard",
+}: CopyToClipbardProps) => {
+  const { mounted, toast } = usePopup("interactive-toast");
+  useEffect(() => {
+    return () => {
+      removePopupManagerContainer();
+    };
+  }, []);
+
+  const handleClick = () => {
+    copyTextToClipboard(text);
+    toast({
+      message: message,
+      duration: duration,
+      position: "top",
+      icon: "copy",
+    });
+  };
+
+  return (
+    <>
+      {mounted && (
+        <Tooltip label="Copy link" position="right">
+          <IconButton
+            variant="tertiary"
+            shape="circle"
+            icon="link"
+            aria-label="copy link to clipboard"
+            onClick={handleClick}
+          />
+        </Tooltip>
+      )}
+    </>
+  );
+};
+
+CopyToClipboard.displayName = "CopyToClipboard";
