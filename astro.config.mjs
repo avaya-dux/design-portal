a1c2be045e5aaa2067cabd264ae928ffb740e0e1
Chunguang Yang <chungguangy@avaya.com>
2023-12-20 17:24:28 -0500

    caching until next deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
-import netlify from "@astrojs/netlify/functions";
-import prefetch from "@astrojs/prefetch";
-import react from "@astrojs/react";
-import svelte from "@astrojs/svelte";
-import vue from "@astrojs/vue";
-import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify/functions"
+import prefetch from "@astrojs/prefetch"
+import react from "@astrojs/react"
+import svelte from "@astrojs/svelte"
+import vue from "@astrojs/vue"
+import { defineConfig } from "astro/config"
 
 // why prefetch: https://docs.astro.build/en/guides/integrations-guide/prefetch/#why-prefetch
 
 export default defineConfig({
   site: "https://design.avayacloud.com/",
-  adapter: netlify(),
+  adapter: netlify({
+    cacheOnDemandPages: true,
+  }),
   output: "server",
   integrations: [react(), svelte(), vue(), prefetch()],
   vite: {
@@ -21,4 +23,4 @@ export default defineConfig({
       ],
     },
   },
-});
+})
