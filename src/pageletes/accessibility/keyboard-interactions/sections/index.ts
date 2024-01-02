2c1b4a45b8b15d40fed515c6bf69ebf685ea125f
Jim Hedges <jahedges@avaya.com>
2024-01-02 15:51:42 -0800

    Adding the keystroke pages.

--- src/pageletes/accessibility/keyboard-interactions/sections/index.ts
+++ src/pageletes/accessibility/keyboard-interactions/sections/index.ts
@@ -1,2 +1,71 @@
 import Intro from "./Intro.astro";
 export { Intro };
+
+import AccordionKeyboardInteractions from "./AccordionKeyboardInteractions.astro";
+export { AccordionKeyboardInteractions };
+
+import ButtonsKeyboardInteractions from "./ButtonsKeyboardInteractions.astro";
+export { ButtonsKeyboardInteractions };
+
+import BreadcrumbsKeyboardInteractions from "./BreadcrumbsKeyboardInteractions";
+export { BreadcrumbsKeyboardInteractions };
+
+import CheckboxKeyboardInteractions from "./CheckboxKeyboardInteractions";
+export { CheckboxKeyboardInteractions };
+
+import ChipKeyboardInteractions from "./ChipKeyboardInteractions";
+export { ChipKeyboardInteractions };
+
+import DropbownKeyboardInteractions from "./DropbownKeyboardInteractions";
+export { DropbownKeyboardInteractions };
+
+import HeaderKeyboardInteractions from "./HeaderKeyboardInteractions";
+export { HeaderKeyboardInteractions };
+
+import ListKeyboardInteractions from "./ListKeyboardInteractions";
+export { ListKeyboardInteractions };
+
+import ModalKeyboardInteractions from "./ModalKeyboardInteractions";
+export { ModalKeyboardInteractions };
+
+import NotificationsKeyboardInteractions from "./NotificationsKeyboardInteractions";
+export { NotificationsKeyboardInteractions };
+
+import PaginationKeyboardInteractions from "./PaginationKeyboardInteractions";
+export { PaginationKeyboardInteractions };
+
+import RadioKeyboardInteractions from "./RadioKeyboardInteractions";
+export {RadioKeyboardInteractions  };
+
+import SelectKeyboardInteractions from "./SelectKeyboardInteractions";
+export {SelectKeyboardInteractions  };
+
+import SheetKeyboardInteractions from "./SheetKeyboardInteractions";
+export {SheetKeyboardInteractions  };
+
+import SidenavigationKeyboardInteractions from "./SidenavigationKeyboardInteractions";
+export {SidenavigationKeyboardInteractions  };
+
+import StepperKeyboardInteractions from "./StepperKeyboardInteractions";
+export {StepperKeyboardInteractions  };
+
+import SwitchKeyboardInteractions from "./SwitchKeyboardInteractions";
+export {SwitchKeyboardInteractions  };
+
+import TablesKeyboardInteractions from "./TablesKeyboardInteractions";
+export {TablesKeyboardInteractions  };
+
+import TabsKeyboardInteractions from "./TabsKeyboardInteractions";
+export {TabsKeyboardInteractions  };
+
+import TextinputKeyboardInteractions from "./TextinputKeyboardInteractions";
+export {TextinputKeyboardInteractions  };
+
+import TooltipKeyboardInteractions from "./TooltipKeyboardInteractions";
+export {TooltipKeyboardInteractions  };
+
+import TreeviewKeyboardInteractions from "./TreeviewKeyboardInteractions";
+export {TreeviewKeyboardInteractions  };
+
+import WidgetKeyboardInteractions from "./WidgetKeyboardInteractions";
+export {WidgetKeyboardInteractions  };
--- src/pageletes/accessibility/keyboard-interactions/static.ts
+++ src/pageletes/accessibility/keyboard-interactions/static.ts
@@ -1,7 +1,29 @@
 import type { TopicsDict } from "components";
 
 type KeyboardInteractionsTopicNames =
-  | "accordion";
+  | "accordion"
+  | "buttons"
+  | "breadcrumbs"
+  | "checkbox"
+  | "chip"
+  | "dropbown"
+  | "header"
+  | "list"
+  | "modal"
+  | "notifications"
+  | "pagination"
+  | "radio"
+  | "select"
+  | "sheet"
+  | "sidenavigation"
+  | "stepper"
+  | "switch"
+  | "tables"
+  | "tabs"
+  | "textinput"
+  | "tooltip"
+  | "treeview"
+  | "widget";
 
 const component = "testing-tools";
 export const topics: TopicsDict<KeyboardInteractionsTopicNames> = {
@@ -9,5 +31,115 @@ export const topics: TopicsDict<KeyboardInteractionsTopicNames> = {
     order: 0,
     id: `${component}-accordion`,
     title: "Accordion",
+  },
+  buttons: {
+    order: 1,
+    id: `${component}-buttons`,
+    title: "Buttons",
+  },
+  breadcrumbs: {
+    order: 2,
+    id: `${component}-breadcrumbs`,
+    title: "Breadcrumbs",
+  },
+  checkbox: {
+    order: 3,
+    id: `${component}-checkbox`,
+    title: "Checkbox",
+  },
+  chip: {
+    order: 4,
+    id: `${component}-chip`,
+    title: "Chip",
+  },
+  dropbown: {
+    order: 5,
+    id: `${component}-dropbown`,
+    title: "Dropbown",
+  },
+  header: {
+    order: 6,
+    id: `${component}-header`,
+    title: "Header",
+  },
+  list: {
+    order: 7,
+    id: `${component}-list`,
+    title: "List",
+  },
+  modal: {
+    order: 8,
+    id: `${component}-modal`,
+    title: "Modal",
+  },
+  notifications: {
+    order: 9,
+    id: `${component}-Notifications`,
+    title: "Notifications",
+  },
+  pagination: {
+    order: 10,
+    id: `${component}-pagination`,
+    title: "Pagination",
+  },
+  radio: {
+    order: 11,
+    id: `${component}-radio`,
+    title: "Radio",
+  },
+  select: {
+    order: 12,
+    id: `${component}-select`,
+    title: "Select",
+  },
+  sheet: {
+    order: 13,
+    id: `${component}-sheet`,
+    title: "Sheet",
+  },
+  sidenavigation: {
+    order: 14,
+    id: `${component}-sidenavigation`,
+    title: "Side Navigation",
+  },
+  stepper: {
+    order: 15,
+    id: `${component}-stepper`,
+    title: "Stepper",
+  },
+  switch: {
+    order: 16,
+    id: `${component}-switch`,
+    title: "Switch",
+  },
+  tables: {
+    order: 17,
+    id: `${component}-tables`,
+    title: "Tables",
+  },
+  tabs: {
+    order: 18,
+    id: `${component}-tabs`,
+    title: "Tabs",
+  },
+  textinput: {
+    order: 19,
+    id: `${component}-textinput`,
+    title: "Text Input",
+  },
+  tooltip: {
+    order: 20,
+    id: `${component}-tooltip`,
+    title: "Tooltip",
+  },
+  treeview: {
+    order: 21,
+    id: `${component}-treeview`,
+    title: "Tree View",
+  },
+  widget: {
+    order: 22,
+    id: `${component}-widget`,
+    title: "Widget",
   },
 };
