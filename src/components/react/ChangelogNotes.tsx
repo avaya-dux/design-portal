305ad10d0577e96b8beb711f42e081269f0bdb81
yangAtSpoken <31546063+yangAtSpoken@users.noreply.github.com>
2024-01-16 13:36:19 -0500

    neo-1982 use github api to pull css library changelog (#470)
    * use githubapi to pull css library changelog
    
    * url redirects; comments; remove host address in release notes
    
    * remove tmp token
    
    * remove key and remove ^ from dependency version
    
    * adjust h4 margin top
    
    * change em style
    
    * remove more em styling to match style in production
    
    * add link to older releases
    
    * minor update

---
+++ src/components/react/ChangelogNotes.tsx
@@ -0,0 +1,30 @@
+import { getOctokit, retrieveGitHubReleaseNotes } from "helpers";
+import { useEffect, useState } from "react";
+import ReactMarkdown from "react-markdown";
+import "./ChangelogNotes.css";
+
+export const ChangelogNotes = ({
+  githubReleaseTag,
+}: {
+  githubReleaseTag: string;
+}) => {
+  const [releaseNotes, setReleaseNotes] = useState<string>();
+
+  useEffect(() => {
+    async function retrieveCSSLibraryGitHubReleaseNotes(tagName: string) {
+      const octokit = getOctokit();
+      const result = await retrieveGitHubReleaseNotes(octokit, tagName);
+      // replace host address, e.g. http://design.avaya.com/, with /
+      setReleaseNotes(result.replaceAll(/https:\/\/.*com\//gi, "/"));
+    }
+    if (githubReleaseTag) {
+      retrieveCSSLibraryGitHubReleaseNotes(githubReleaseTag);
+    }
+  }, [githubReleaseTag]);
+
+  return (
+    <div className="changes__wrapper">
+      <ReactMarkdown>{releaseNotes}</ReactMarkdown>
+    </div>
+  );
+};
---
+++ src/helpers/githubapi.test.ts
@@ -0,0 +1,24 @@
+import {
+  getGithubReleases,
+  getOctokit,
+  retrieveGitHubReleaseNotes,
+} from "./githubapi";
+
+const key = "put in personal access key";
+// These tests are integration tests, thus they are skipped during pipeline runs.
+// To run these tests manually, make sure key is valid and remove .skip from describe.skip
+describe.skip("integretion test", () => {
+  test(getGithubReleases.name, async () => {
+    const octokit = getOctokit(key);
+    const releases = await getGithubReleases(octokit);
+    console.log(releases);
+    // The page size of the api is 20 by default
+    expect(releases).toHaveLength(20);
+  });
+
+  test(retrieveGitHubReleaseNotes.name, async () => {
+    const octokit = getOctokit(key);
+    const result = await retrieveGitHubReleaseNotes(octokit, "v3.78.0");
+    expect(result).not.toBeNull();
+  });
+});
