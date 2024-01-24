868737184ae1c3d83bf80e41db87ab51cf00031c
Jim Hedges <jahedges@avaya.com>
2024-01-24 09:43:02 -0800

    Page update and added playground.

--- src/pageletes/components/notifications/sections/PlaygroundImplementation.tsx
+++ src/pageletes/components/notifications/sections/PlaygroundImplementation.tsx
@@ -2,11 +2,8 @@ import {
   Notification,
   Radio,
   RadioGroup,
-  Select,
-  type NotificationProps,
-  SelectOption,
 } from "@avaya/neo-react";
-import { useMemo, useState, type Dispatch, type SetStateAction } from "react";
+import { useMemo, useState } from "react";
 
 import { Playground } from "components";
 
@@ -20,145 +17,57 @@ export const sandbox =
 export const storybook =
   "https://neo-react-library-storybook.netlify.app/?path=/story/components-notification";
 
-type TypeOption = "info" | "success" | "warning" | "alert" | "event";
-type LineOption = "one" | "two";
-type RightSideOption = "close" | "timer" | "button";
+type TypeOption = "general" | "inline";
 
-const getIcon = (type: TypeOption) => {
-  switch (type) {
-    case "event":
-      return "copy";
-    case "success":
-      return "check";
-    default:
-      return type;
-  }
-};
-
-const getHeader = (type: TypeOption) => {
-  const s = type as string;
-  return s.charAt(0).toUpperCase() + s.slice(1);
-};
-const getDescription = (type: TypeOption, rightSideOption: RightSideOption) => {
-  if (rightSideOption == "timer") {
-    return "A timer display";
-  }
-
-  if (rightSideOption == "button") {
-    return "Two custom actions";
-  }
-
-  switch (type) {
-    case "info":
-      return "An info with close action";
-    case "success":
-      return "A success with close action";
-    case "warning":
-      return "A warning with close action";
-    case "alert":
-      return "An alert with close action";
-    case "event":
-      return "An event with close action";
-    default:
-      return "";
-  }
-};
-
-const getAction = (
-  rightSideOption: RightSideOption,
-  setClosed: Dispatch<SetStateAction<boolean>>,
-) => {
-  switch (rightSideOption) {
-    case "close":
-      return {
-        onClick: () => {
-          alert("Close Clicked");
-          setClosed(true);
-        },
-      };
-    case "timer":
-      return { count: "12:34:56" };
-    default:
-      return {
-        buttons: [
-          { children: "Edit", onClick: () => alert("Edit Clicked") },
-          { children: "Alert", onClick: () => alert("Alert Clicked") },
-        ],
-      };
-  }
-};
 
 export const PlaygroundImplementation = () => {
-  const [type, setType] = useState<TypeOption>("info");
-  const [lineOption, setLineOption] = useState<LineOption>("one");
-  const [rightSideOption, setRightSideOption] =
-    useState<RightSideOption>("close");
-  const [closed, setClosed] = useState(false);
+  const [type, setType] = useState<TypeOption>("inline");
 
-  const [element, react, html] = useMemo(() => {
-    const icon = getIcon(type);
-    const action = getAction(rightSideOption, setClosed);
-    const description = getDescription(type, rightSideOption);
-    let props: NotificationProps = { type, icon, description, action };
-    const header = getHeader(type);
-    if (lineOption == "two") {
-      props = { ...props, header };
-    }
-    const element = <Notification {...props} />;
+  const [isInline, element, react, html] = useMemo(() => {
+    const isInline = type ==="inline"
+    const element = <Notification
+      type="info"
+      header="Info"
+      description="This is the general inplementation"
+      actions={{
+        actionButtons: {
+          buttons: [
+            {
+              children: 'Edit',
+              onClick: function Ga(){}
+            },
+            {
+              children: 'Alert',
+              onClick: function Ga(){}
+            }
+          ]
+        }
+      }}
+        />;
     return [
+      isInline,
       element,
       prettyPrintReactElementToString(element),
       prettyPrintReactElementToHtml(element),
     ];
-  }, [type, lineOption, rightSideOption, setClosed]);
+  }, [type]);
 
   return (
     <Playground
       options={
         <Playground.OptionsContainer>
           <Playground.OptionsSection title="Type">
-            <Select
+            <RadioGroup
+              groupName="type"
               aria-label="Notification Type"
-              value={type}
-              onChange={(value: string) => {
-                setType(value as TypeOption);
-                setClosed(false);
+              selected={type}
+              onChange={(e: { target: {value: string } }) => {
+                setType(e.target.value as TypeOption);
               }}
             >
-              <SelectOption value="info">Info</SelectOption>
-              <SelectOption value="success">Success</SelectOption>
-              <SelectOption value="warning">Warning</SelectOption>
-              <SelectOption value="alert">Alert</SelectOption>
-              <SelectOption value="event">Event</SelectOption>
-            </Select>
-          </Playground.OptionsSection>
-          <Playground.OptionsSection title="Line of texts">
-            <RadioGroup
-              groupName="line-of-texts"
-              selected={lineOption}
-              onChange={(e: { target: { value: string } }) => {
-                setLineOption(e.target.value as LineOption);
-                setClosed(false);
-              }}
-            >
-              <Radio value="one">1</Radio>
-              <Radio value="two">2</Radio>
-            </RadioGroup>
-          </Playground.OptionsSection>
-
-          <Playground.OptionsSection title="Right Side">
-            <RadioGroup
-              groupName="right-side-options"
-              selected={rightSideOption}
-              onChange={(e: { target: { value: string } }) => {
-                setRightSideOption(e.target.value as RightSideOption);
-                setClosed(false);
-              }}
-            >
-              <Radio value="button">Button</Radio>
-              <Radio value="timer">Timer</Radio>
-              <Radio value="close">Close</Radio>
-            </RadioGroup>
+              <Radio value="general">General</Radio>
+              <Radio value="inline">Inline</Radio>
+           </RadioGroup>
           </Playground.OptionsSection>
         </Playground.OptionsContainer>
       }
@@ -169,7 +78,27 @@ export const PlaygroundImplementation = () => {
         storybook,
       }}
     >
-      <div style={{ width: "100%" }}>{closed ? null : element}</div>
+      <div style={{ width: "100%" }}>{
+      isInline ? <Notification
+            type="info"
+            header="Info"
+            description="This is inline inplimentation"
+            isInline
+            actions={{
+              actionButtons: {
+                buttons: [
+                  {
+                    children: 'Edit',
+                    onClick: function Ga(){}
+                  },
+                  {
+                    children: 'Alert',
+                    onClick: function Ga(){}
+                  }
+                ]
+              }
+            }}
+      /> : element}</div>
     </Playground>
   );
 };
