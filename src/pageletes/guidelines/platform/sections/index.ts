507b6507ab119216b83b08cf0e6acceb57226e59
JoeS <55896546+joe-s-avaya@users.noreply.github.com>
2024-02-09 19:02:07 -0600

    NEO-1968 // update Guidelines pages to handle smaller screen sizes (#492)
    * update guidelines overview page
    
    * fix guidelines colors page
    
    * update guidelines localization page
    
    * update guidelines motion page
    
    * update guidelines platform pages
    
    * update guidelines sound page
    
    * update guidelines typography

--- src/pageletes/guidelines/platform/sections/index.ts
+++ src/pageletes/guidelines/platform/sections/index.ts
@@ -16,14 +16,5 @@ export { iOS };
 import Windows from "./Windows.astro";
 export { Windows };
 
-import SectionLayout from "./sections-frame-layout/SectionLayout.astro";
-export { SectionLayout };
-
-import TwoColumnLayout from "./sections-frame-layout/TwoColumnLayout.astro";
-export { TwoColumnLayout };
-
 import Sections from "./Sections.astro";
 export { Sections };
-
-import SectionList from "./sections-frame-layout/SectionList.astro";
-export { SectionList };
