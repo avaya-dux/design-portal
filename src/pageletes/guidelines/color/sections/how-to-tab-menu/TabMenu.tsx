146767d9f5473ec373e2b8e84ecfd49973cd84c1
JoeS <55896546+joe-s-avaya@users.noreply.github.com>
2024-02-12 16:28:12 -0600

    update Guidelines/Colors per Matt's request (#497)
    * make three column layout component more flexible
    
    * simplify info block component
    
    * resolve build warnings by fixing ts types
    
    * update images

--- src/pageletes/guidelines/color/sections/how-to-tab-menu/TabMenu.tsx
+++ src/pageletes/guidelines/color/sections/how-to-tab-menu/TabMenu.tsx
@@ -1,16 +1,19 @@
 import { Tabs, TabList, Tab, TabPanels, TabPanel } from "@avaya/neo-react";
 
-export const TabMenu = (props) => {
+export const TabMenu = (props: { panels: string[] }) => {
   return (
     <Tabs>
       <TabList>
-        {props.panels.map((panel: string) => (
-          <Tab id={panel}>{panel.charAt(0).toUpperCase() + panel.slice(1)}</Tab>
+        {props.panels.map((panel: string, i: number) => (
+          <Tab id={panel} key={`${i}-${panel}`}>
+            {panel.charAt(0).toUpperCase() + panel.slice(1)}
+          </Tab>
         ))}
       </TabList>
+
       <TabPanels>
         {props.panels.map((panel: string) => (
-          <TabPanel>{props[panel]}</TabPanel>
+          <TabPanel>{props[panel as keyof typeof props]}</TabPanel>
         ))}
       </TabPanels>
     </Tabs>
