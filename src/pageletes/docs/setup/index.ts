e05812b5652b671b9ba914a3b384f0d0f905d649
Joe Sebast <joe.sebast.avaya@gmail.com>
2024-02-15 13:47:58 -0600

    update first half of setup page

--- src/pageletes/docs/setup/index.ts
+++ src/pageletes/docs/setup/index.ts
@@ -1,8 +1,1 @@
-import Intro from "./Intro.astro";
-export { Intro };
-
-import CSS from "./CSS.astro";
-export { CSS };
-
-import React from "./React.astro";
-export { React };
+export * from "./sections";
---
+++ src/pageletes/docs/setup/sections/index.ts
@@ -0,0 +1,8 @@
+import Intro from "./Intro.astro";
+export { Intro };
+
+import GettingStarted from "./GettingStarted.astro";
+export { GettingStarted };
+
+import NeoReactFramework from "./NeoReactFramework.astro";
+export { NeoReactFramework };
