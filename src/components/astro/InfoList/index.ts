d764db5a1ddbfef4b99cc35af460967c4db2c225
Jim Hedges <31745825+jahedges@users.noreply.github.com>
2024-02-16 13:06:44 -0800

    NEO-1944 update to use info list (#502)
    * Button page updates.
    * Fixed Typo
    * Added "upper-alpha" to InfoLists
    * Adjusted the size of the lists under images as per Matt
    * Created wrapper for image captions that are a specific formatted InfoList

--- src/components/astro/InfoList/index.ts
+++ src/components/astro/InfoList/index.ts
@@ -1,6 +1,9 @@
 import BulletInfoList from "./BulletInfoList.astro";
 export { BulletInfoList };
 
+import CaptionInfoList from "./CaptionInfoList.astro";
+export { CaptionInfoList };
+
 import InfoList from "./InfoList.astro";
 export { InfoList };
 
