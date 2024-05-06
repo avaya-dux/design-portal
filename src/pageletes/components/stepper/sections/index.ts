3f29dab81034fa216adbed8e0780a09377acb59e
Joe Sebast <joe.sebast.avaya@gmail.com>
2024-05-06 13:48:28 -0500

    add all pages
    and most of the content

---
+++ src/pageletes/components/stepper/sections/index.ts
@@ -0,0 +1,23 @@
+import Intro from "./Intro.astro";
+export { Intro };
+
+import Playground from "./Playground.astro";
+export { Playground };
+
+import Anatomy from "./Anatomy.astro";
+export { Anatomy };
+
+import States from "./States.astro";
+export { States };
+
+import Types from "./Types.astro";
+export { Types };
+
+import Behavior from "./Behavior.astro";
+export { Behavior };
+
+import Specs from "./Specs.astro";
+export { Specs };
+
+import KeyboardInteractions from "./KeyboardInteractions.astro";
+export { KeyboardInteractions };
---
+++ src/pageletes/components/stepper/static.ts
@@ -0,0 +1,38 @@
+import type { TopicsDict } from "components";
+
+type StepperTopicNames =
+  | "playground"
+  | "anatomy"
+  | "states"
+  | "types"
+  | "behavior"
+  | "specs"
+  | "keyboard"
+  | "related";
+
+export const topics: TopicsDict<StepperTopicNames> = {
+  playground: {
+    order: 0,
+    id: "stepper-playground",
+    title: "Interactive Playground",
+  },
+  anatomy: { order: 1, id: "stepper-anatomy", title: "Anatomy" },
+  states: { order: 2, id: "stepper-states", title: "States" },
+  types: { order: 3, id: "stepper-type", title: "Type" },
+  behavior: { order: 4, id: "stepper-behavior", title: "Behavior" },
+  specs: {
+    order: 5,
+    id: "stepper-specs",
+    title: "Specs",
+  },
+  keyboard: {
+    order: 6,
+    id: "stepper-keyboard",
+    title: "Keyboard Interactions",
+  },
+  related: {
+    order: 7,
+    id: "stepper-related-content",
+    title: "Related Content",
+  },
+};
