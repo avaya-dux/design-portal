632be7cea04d6ff855ab35752dbb174ebc54dc57
JoeS <55896546+joe-s-avaya@users.noreply.github.com>
2024-10-03 09:09:32 -0500

    Add internal docs on displaying images (#581)
    * single image example added
    
    * side by side images
    
    * format
    
    * add bottom aligned image example
    
    * format
    
    * simplify start command
    
    * move files around and improve docs
    
    * add another example
    
    * more examples
    
    * adding do and dont examples
    
    * fix border issue
    
    * update bottom aligned images per Matt's instructions
    
    * update to use v2 image only
    
    * add a quick note

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,35 @@
-import netlify from "@astrojs/netlify"
-import prefetch from "@astrojs/prefetch"
-import react from "@astrojs/react"
-import svelte from "@astrojs/svelte"
-import vue from "@astrojs/vue"
-import { defineConfig } from "astro/config"
+import netlify from "@astrojs/netlify";
+import prefetch from "@astrojs/prefetch";
+import react from "@astrojs/react";
+import svelte from "@astrojs/svelte";
+import vue from "@astrojs/vue";
+import { defineConfig } from "astro/config";
 
 // why prefetch: https://docs.astro.build/en/guides/integrations-guide/prefetch/#why-prefetch
 
 export default defineConfig({
-  site: "https://design.avayacloud.com/",
-  adapter: netlify(),
-  output: "server",
-  integrations: [react(), svelte(), vue(), prefetch()],
-  redirects: {
-    '/components/web/navbar-web': '/components/header',
-    '/components/web/tables-web': '/components/table',
-    '/components/web/switch-web': '/components/switch',
-    '/components/web/widget-web': '/components/widget',
-    '/components/icons': '/components/icon',
-    '/components/web/dropdown-web': '/components/dropdown',
-    '/components/web/checkbox-web': '/components/checkbox',
-    '/components/web/pagination-web': '/components/pagination'
-  },
-  vite: {
-    ssr: {
-      noExternal: [
-        "avaya-neo-react",
-        "prism-react-renderer",
-        "react-element-to-jsx-string",
-        "octokit",
-      ],
-    },
-  },
-})
+	site: "https://design.avayacloud.com/",
+	adapter: netlify(),
+	output: "server",
+	integrations: [react(), svelte(), vue(), prefetch()],
+	redirects: {
+		"/components/web/navbar-web": "/components/header",
+		"/components/web/tables-web": "/components/table",
+		"/components/web/switch-web": "/components/switch",
+		"/components/web/widget-web": "/components/widget",
+		"/components/icons": "/components/icon",
+		"/components/web/dropdown-web": "/components/dropdown",
+		"/components/web/checkbox-web": "/components/checkbox",
+		"/components/web/pagination-web": "/components/pagination",
+	},
+	vite: {
+		ssr: {
+			noExternal: [
+				"avaya-neo-react",
+				"prism-react-renderer",
+				"react-element-to-jsx-string",
+				"octokit",
+			],
+		},
+	},
+});
