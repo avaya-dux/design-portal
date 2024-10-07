f91315bbdbba007b93ace2fd4daa83e89fddc830
Joe Sebast <joe.sebast.avaya@gmail.com>
2024-10-07 14:08:40 -0500

    create new page

---
+++ src/pageletes/components/drawer/sections/index.ts
@@ -0,0 +1,17 @@
+import Intro from "./Intro.astro";
+export { Intro };
+
+// import PaginationPlayground from "./PaginationPlayground.astro";
+// export { PaginationPlayground };
+
+// import Anatomy from "./Anatomy.astro";
+// export { Anatomy };
+
+// import Behavior from "./Behavior.astro";
+// export { Behavior };
+
+// import Specs from "./Specs.astro";
+// export { Specs };
+
+// import KeyboardInteractions from "./KeyboardInteractions.astro";
+// export { KeyboardInteractions };
---
+++ src/pageletes/components/drawer/static.ts
@@ -0,0 +1,37 @@
+import type { TopicsDict } from "components";
+
+type DrawerTopicNames =
+	| "playground"
+	| "anatomy"
+	| "behavior"
+	| "specs"
+	| "keyboard";
+
+const component = "drawer";
+export const topics: TopicsDict<DrawerTopicNames> = {
+	playground: {
+		order: 0,
+		id: `${component}-playground`,
+		title: "Interactive Playground",
+	},
+	anatomy: {
+		order: 1,
+		id: `${component}-anatomy`,
+		title: "Anatomy",
+	},
+	behavior: {
+		order: 2,
+		id: `${component}-checkbox`,
+		title: "Behavior",
+	},
+	specs: {
+		order: 3,
+		id: `${component}-specs`,
+		title: "Specs",
+	},
+	keyboard: {
+		order: 4,
+		id: `${component}-keyboard-interactions`,
+		title: "Keyboard Interactions",
+	},
+};
