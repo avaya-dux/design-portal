070efbdb7bbbe9a44e771ed09f96b9c772c57071
Joe Sebast <joe.sebast.avaya@gmail.com>
2024-10-08 14:43:24 -0500

    add behavior section

--- src/pageletes/components/drawer/sections/index.ts
+++ src/pageletes/components/drawer/sections/index.ts
@@ -7,8 +7,8 @@ export { Intro };
 import Anatomy from "./Anatomy.astro";
 export { Anatomy };
 
-// import Behavior from "./Behavior.astro";
-// export { Behavior };
+import Behavior from "./Behavior.astro";
+export { Behavior };
 
 // import Specs from "./Specs.astro";
 // export { Specs };
