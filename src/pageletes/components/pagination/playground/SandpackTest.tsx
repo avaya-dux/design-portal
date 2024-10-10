99fa13fd2dbddeeb01ac69d0c655be6b9186341d
Joe Sebast <joe.sebast.avaya@gmail.com>
2024-10-10 09:57:33 -0500

    working on HTML example

--- src/pageletes/components/pagination/playground/SandpackTest.tsx
+++ src/pageletes/components/pagination/playground/SandpackTest.tsx
@@ -7,7 +7,62 @@ import {
 
 import "./SandpackTest.css";
 
-const codeString = `
+const htmlCodeString = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Parcel Sandbox</title>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="@avaya/neo/neo/dist/css/neo/neo.min.css" />
+  </head>
+
+  <body>
+    <main>
+      <div class="neo-pagination__row" id="pagination-:R0:">
+        <nav class="neo-pagination neo-pagination__condensed">
+          <button
+            type="button"
+            disabled
+            aria-label="previous"
+            class="neo-btn-square neo-pagination__arrow-btn neo-icon-chevron-left"
+          ></button>
+          <div class="pagination__go-to-page">
+            <div data-testid="NeoInputWrapper-root" class="neo-form-control">
+              <div
+                data-testid="NeoInputWrapper-group-root"
+                class="neo-input-group"
+              >
+                <div class="neo-input-editable__wrapper">
+                  <input
+                    class="neo-input"
+                    id=":Rd:"
+                    tabindex="0"
+                    type="number"
+                    aria-label="Go to page"
+                    pattern="[0-9]+"
+                    min="1"
+                    max="5"
+                    value="1"
+                  />
+                </div>
+              </div>
+            </div>
+            <span> / 5 </span>
+            <span> pages </span>
+          </div>
+          <button
+            type="button"
+            aria-label="next"
+            class="neo-btn-square neo-pagination__arrow-btn neo-icon-chevron-right"
+          ></button>
+        </nav>
+      </div>
+    </main>
+  </body>
+</html>
+`.trim();
+
+const reactCodeString = `
 import { useState } from "react";
 import { Pagination, NeoThemeProvider } from "@avaya/neo-react";
 
@@ -40,7 +95,7 @@ export default function App() {
   );
 }`.trim();
 
-const stylesString = `
+const cssString = `
 body {
   margin: 0;
 }
@@ -51,37 +106,97 @@ body {
 
 export const SandpackTest = () => {
 	return (
-		<div className="playground-container">
-			<SandpackProvider
-				template="react-ts"
-				theme="auto"
-				customSetup={{
-					dependencies: {
-						"@avaya/neo-react": "1.3.5",
-					},
-				}}
-				files={{
-					"App.tsx": codeString,
-					"styles.css": stylesString,
-				}}
-			>
-				<div className="playground-examples">
-					<SandpackPreview
-						actionsChildren={
-							<Button
-								style={{ height: 28 }}
-								onClick={() => window.alert("Bug reported!")}
-							>
-								Report bug
-							</Button>
-						}
-					/>
+		<div>
+			<div className="playground-container">
+				<SandpackProvider
+					template="react-ts"
+					theme="auto"
+					customSetup={{
+						dependencies: {
+							"@avaya/neo-react": "1.3.5",
+						},
+					}}
+					files={{
+						"App.tsx": reactCodeString,
+						"styles.css": cssString,
+					}}
+				>
+					<div className="playground-examples">
+						<SandpackPreview
+							actionsChildren={
+								<>
+									<Button
+										style={{ height: 28 }}
+										onClick={() =>
+											window.alert("TODO: implement: 'Bug reported!'")
+										}
+									>
+										COPY EXAMPLE CODE
+									</Button>
 
-					<div className="playground-code">
-						<SandpackCodeEditor />
+									<Button
+										style={{ height: 28 }}
+										onClick={() =>
+											window.alert("TODO: implement: 'Bug reported!'")
+										}
+									>
+										Report bug
+									</Button>
+								</>
+							}
+						/>
+
+						<div className="playground-code">
+							<SandpackCodeEditor />
+						</div>
 					</div>
-				</div>
-			</SandpackProvider>
+				</SandpackProvider>
+			</div>
+
+			<div className="playground-container">
+				<SandpackProvider
+					template="static"
+					theme="auto"
+					customSetup={{
+						dependencies: {
+							"@avaya/neo": "3.81.16",
+						},
+					}}
+					files={{
+						"index.html": htmlCodeString,
+					}}
+				>
+					<div className="playground-examples">
+						<SandpackPreview
+							actionsChildren={
+								<>
+									<Button
+										style={{ height: 28 }}
+										onClick={() =>
+											window.alert("TODO: implement: 'Bug reported!'")
+										}
+									>
+										COPY EXAMPLE CODE
+									</Button>
+
+									<Button
+										style={{ height: 28 }}
+										onClick={() =>
+											window.alert("TODO: implement: 'Bug reported!'")
+										}
+									>
+										Report bug
+									</Button>
+								</>
+							}
+						/>
+
+						<div className="playground-code">
+							<SandpackCodeEditor />
+						</div>
+					</div>
+				</SandpackProvider>
+			</div>
 		</div>
 	);
 };
