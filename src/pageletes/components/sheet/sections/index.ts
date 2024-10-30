957dccba864d0bfb518f7619e92756a7e18bb0e6
Mohammad Razian <raziandev@Mohammads-MacBook-Air.local>
2024-10-30 07:10:26 +0100

    Removed content from Sheet page, replaced with Table re-directing to Modal and Drawer pages, removed Sheet content from codebase

--- src/pageletes/components/sheet/sections/index.ts
+++ src/pageletes/components/sheet/sections/index.ts
@@ -1,23 +1,2 @@
 import Intro from "./Intro.astro";
 export { Intro };
-
-import Anatomy from "./Anatomy.astro";
-export { Anatomy };
-
-import Behavior from "./Behavior.astro";
-export { Behavior };
-
-import Specs from "./Specs.astro";
-export { Specs };
-
-import KeyboardInteractions from "./KeyboardInteractions.astro";
-export { KeyboardInteractions };
-
-import KeyboardInteractionsTable from "./KeyboardInteractionsTable.astro";
-export { KeyboardInteractionsTable };
-
-import Related from "./Related.astro";
-export { Related };
-
-import SheetPlayground from "./SheetPlayground.astro";
-export { SheetPlayground };
